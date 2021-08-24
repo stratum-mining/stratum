d35526f9890dad04bedfe0b7ee023fb38f7f6afc
RJ Rybarczyk <rj@rybar.tech>
2021-08-24 15:24:24 +0000

    Implement display for all Error variants

--- protocols/v2/binary-sv2/serde-sv2/src/error.rs
+++ protocols/v2/binary-sv2/serde-sv2/src/error.rs
@@ -15,25 +15,24 @@ pub enum Error {
     // Mutex<T> might return an error because the mutex is poisoned, or the
     // Deserialize impl for a struct may return an error because a required
     // field is missing.
-    Message(String),
-
-    StringLenBiggerThan256,
-    InvalidUtf8,
-    LenBiggerThan16M,
-    LenBiggerThan255,
-    LenBiggerThan32,
-    LenBiggerThan64K,
-    WriteError,
-    ReadError,
-    InvalidBoolSize(usize),
     InvalidBool(u8),
-    InvalidU256(usize),
+    InvalidBoolSize(usize),
     InvalidSignatureSize(usize),
     InvalidU16Size(usize),
     InvalidU24Size(usize),
     InvalidU32Size(usize),
     InvalidU64Size(usize),
+    InvalidU256(usize),
+    InvalidUtf8,
+    Message(String),
+    LenBiggerThan16M,
+    LenBiggerThan32,
+    LenBiggerThan64K,
+    LenBiggerThan255,
+    ReadError,
+    StringLenBiggerThan256,
     U24TooBig(u32),
+    WriteError,
 }
 
 impl ser::Error for Error {
@@ -51,10 +50,59 @@ impl de::Error for Error {
 impl Display for Error {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         match self {
+            Error::InvalidBool(b) => formatter.write_fmt(format_args!(
+                "Invalid Boolean. Expected Boolean, got `{}`.",
+                b
+            )),
+            Error::InvalidBoolSize(n) => {
+                formatter.write_fmt(format_args!("Invalid Boolean size: `{}`.", n))
+            }
+            Error::InvalidSignatureSize(n) => {
+                formatter.write_fmt(format_args!("Invalid signature size: `{}`.", n))
+            }
+            Error::InvalidU16Size(n) => formatter.write_fmt(format_args!(
+                "Invalid size. Expected u16 number, got number with size of `{}`.",
+                n
+            )),
+            Error::InvalidU24Size(n) => formatter.write_fmt(format_args!(
+                "Invalid size. Expected u24 number, got number with size of `{}`.",
+                n
+            )),
+            Error::InvalidU32Size(n) => formatter.write_fmt(format_args!(
+                "Invalid size. Expected u32 number, got number with size of `{}`.",
+                n
+            )),
+            Error::InvalidU64Size(n) => formatter.write_fmt(format_args!(
+                "Invalid size. Expected u64 number, got number with size of `{}`.",
+                n
+            )),
+            Error::InvalidU256(n) => formatter.write_fmt(format_args!(
+                "Invalid number. Expected type u256, got `{}`.",
+                n
+            )),
+            Error::InvalidUtf8 => formatter.write_str("Invalid utf8."),
             Error::Message(msg) => formatter.write_str(msg),
-            Error::WriteError => formatter.write_str("write error"),
-            Error::ReadError => formatter.write_str("read error"),
-            _ => formatter.write_str("TODO display not implemented"),
+            Error::LenBiggerThan16M => {
+                formatter.write_str("Length is too long, must be less than 16M.")
+            }
+            Error::LenBiggerThan32 => {
+                formatter.write_str("Length is too long, must be less than 32.")
+            }
+            Error::LenBiggerThan64K => {
+                formatter.write_str("Length is too long, must be less than 64K.")
+            }
+            Error::LenBiggerThan255 => {
+                formatter.write_str("Length is too long, must be less than 255.")
+            }
+            Error::ReadError => formatter.write_str("Read error."),
+            Error::StringLenBiggerThan256 => {
+                formatter.write_str("String length is too long, must be less than 256.")
+            }
+            Error::U24TooBig(n) => formatter.write_fmt(format_args!(
+                "Invalid size. Expected u24 number, got number with size of `{}`.",
+                n
+            )),
+            Error::WriteError => formatter.write_str("Write error."),
         }
     }
 }
