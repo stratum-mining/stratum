9f30d04ebce3c42815881bf1edffd3f1baba63a0
RJ Rybarczyk <rj@rybar.tech>
2021-08-25 17:08:34 +0000

    Move sv2 header check back to root

--- sv2-header-check.sh
+++ sv2-header-check.sh
@@ -1,14 +1,16 @@
-set -e
+# set -e
 cargo install cbindgen --force bts
 cbindgen -V
 
 cd ./protocols/v2/sv2-ffi
 SHA1_1=$(sha1sum sv2.h)
+echo $SHA1_1
 cd ../../..
 
 BUILD_SCRIPT="./build_header.sh"
 sh ./"$BUILD_SCRIPT"
 
 SHA1_2=$(sha1sum sv2.h)
+echo $SHA1_2
 
 [ "$SHA1_1" = "$SHA1_2" ]; echo "$?"
