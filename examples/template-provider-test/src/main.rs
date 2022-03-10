6a28629072b9a0b95c655bf4014237b31440b7f5
fi3 <email@email.org>
2022-03-10 17:46:57 +0100

    Remove test_0 from TP example cause no more relevant

--- examples/template-provider-test/src/main.rs
+++ examples/template-provider-test/src/main.rs
@@ -1,7 +1,6 @@
 use async_channel::{Receiver, Sender};
-use async_std::io::{ReadExt, WriteExt};
 use async_std::net::TcpStream;
-use codec_sv2::{Frame, StandardDecoder, StandardEitherFrame, StandardSv2Frame, Sv2Frame};
+use codec_sv2::{Frame, StandardEitherFrame, StandardSv2Frame, Sv2Frame};
 use messages_sv2::parsers::{IsSv2Message, TemplateDistribution};
 use messages_sv2::template_distribution_sv2::SubmitSolution;
 use network_helpers::PlainConnection;
@@ -14,39 +13,10 @@ pub type EitherFrame = StandardEitherFrame<Message>;
 
 #[async_std::main]
 async fn main() {
-    //test_0().await;
     test_1().await;
 }
 #[allow(unused)]
-async fn test_0() {
-    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8442);
-    let mut stream = TcpStream::connect(socket).await.unwrap();
-    let mut decoder = StandardDecoder::<Message>::new();
-    stream.write_all(b"test").await.unwrap();
-    loop {
-        let writable = decoder.writable();
-        match stream.read_exact(writable).await {
-            Ok(_) => {
-                if let Ok(mut frame) = decoder.next_frame() {
-                    // this is how mesg-type should be retreived
-                    let _msg_type = frame.get_header().unwrap().msg_type();
-                    // but I force it to be 113 (0x71) in order the decode the correct message type
-                    let msg_type = 113;
-                    let payload = frame.payload();
-                    match (msg_type, payload).try_into() {
-                        Ok(TemplateDistribution::NewTemplate(m)) => println!("{:#?}", m),
-                        Ok(_) => panic!(),
-                        Err(_) => panic!(),
-                    }
-                }
-            }
-            Err(_) => {
-                let _ = stream.shutdown(async_std::net::Shutdown::Both);
-                break;
-            }
-        }
-    }
-}
+
 fn hex_to_bytes(s: &str) -> Option<Vec<u8>> {
     if s.len() % 2 == 0 {
         (0..s.len())
@@ -81,8 +51,6 @@ async fn test_1() {
     let channel_bit = submit_solution.channel_bit();
     // Below is the right message type
     let message_type = submit_solution.message_type();
-    // But I use 76 cause code on core is old version
-    // let message_type = 76;
     let frame: StdFrame =
         Sv2Frame::from_message(submit_solution, message_type, extension_type, channel_bit).unwrap();
     let frame: EitherFrame = frame.try_into().unwrap();
