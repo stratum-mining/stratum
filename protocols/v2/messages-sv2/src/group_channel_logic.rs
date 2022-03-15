8f3dcf9381ad537b1c744916a2021f06b921306d
RJ Rybarczyk <rj@rybar.tech>
2022-03-15 17:27:52 -0400

    Test UpdateWithGroups adds a new group channel

--- protocols/v2/messages-sv2/src/group_channel_logic.rs
+++ protocols/v2/messages-sv2/src/group_channel_logic.rs
@@ -58,4 +58,83 @@ mod tests {
         assert_eq!(expect.ids, actual.ids);
         assert_eq!(expect.extranonces, actual.extranonces);
     }
+
+    #[test]
+    fn adds_a_new_group_channel_to_upstream_with_groups() {
+        let mut expect_upstream_with_groups = UpstreamWithGroups {
+            groups: HashMap::new(),
+            ids: Id::new(),
+            extranonces: Extranonce::new(),
+        };
+
+        let mut upstream_with_groups = UpstreamWithGroups {
+            groups: HashMap::new(),
+            ids: Id::new(),
+            extranonces: Extranonce::new(),
+        };
+        assert!(upstream_with_groups.groups.is_empty());
+
+        // Call `new_group_channel` for the first time on the `UpstreamWithGroups` instance
+        let actual = &upstream_with_groups.new_group_channel();
+
+        // Return of `new_group_channel` is the latest group channel id, which is the last group
+        // channel id incremented by 1
+        assert_eq!(&1, actual);
+
+        // Assert that the `extranonces` field does not change
+        assert_eq!(
+            expect_upstream_with_groups.extranonces,
+            upstream_with_groups.extranonces
+        );
+
+        // Increments the `Id` state from 0 to 1
+        expect_upstream_with_groups.ids.next();
+        assert_eq!(expect_upstream_with_groups.ids, upstream_with_groups.ids);
+
+        // Populates previously empty `groups` hashmap
+        assert!(!upstream_with_groups.groups.is_empty());
+
+        // Verify that the group hashmap is 1) populated with a length of 1, 2) the key is `1`, and
+        // 3) the value is an Id with a state of 0
+        let groups_hashmap = &upstream_with_groups.groups;
+
+        assert_eq!(1, groups_hashmap.len());
+        assert!(&groups_hashmap.contains_key(&1));
+
+        let actual_value = (&groups_hashmap).get(&1).unwrap();
+        let expect_value = Id::new();
+        assert_eq!(expect_value, *actual_value);
+
+        // Call `new_group_channel` for the second time on the `UpstreamWithGroups` instance
+        let actual = &upstream_with_groups.new_group_channel();
+
+        // Return of `new_group_channel` is the latest group channel id, which is the last group
+        // channel id incremented by 1
+        assert_eq!(&2, actual);
+
+        // Assert that the `extranonces` field does not change
+        assert_eq!(
+            expect_upstream_with_groups.extranonces,
+            upstream_with_groups.extranonces
+        );
+
+        // Increments the `Id` state from 1 to 2
+        expect_upstream_with_groups.ids.next();
+        assert_eq!(expect_upstream_with_groups.ids, upstream_with_groups.ids);
+
+        // Verify that the group hashmap is 1) populated with a length of 2, 2) the key is `1`, and
+        // 3) the value is an Id with a state of 0
+        let groups_hashmap = &upstream_with_groups.groups;
+
+        assert_eq!(2, groups_hashmap.len());
+        assert!(&groups_hashmap.contains_key(&1));
+
+        let actual_value = (&groups_hashmap).get(&1).unwrap();
+        let expect_value = Id::new();
+        assert_eq!(expect_value, *actual_value);
+
+        assert!(&groups_hashmap.contains_key(&2));
+        let actual_value = (&groups_hashmap).get(&2).unwrap();
+        assert_eq!(expect_value, *actual_value);
+    }
 }
