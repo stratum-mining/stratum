b4aa6f66e629fd3cd2bdd7271286a72b4d6dafda
fi3 <email@email.org>
2022-07-12 13:48:19 +0200

    Update minig-proxy to use a BufferPool instead then a simple vec

--- roles/v2/mining-proxy/src/lib/upstream_mining.rs
+++ roles/v2/mining-proxy/src/lib/upstream_mining.rs
@@ -277,7 +277,7 @@ impl UpstreamMiningNode {
         );
         match next_message_to_send {
             Ok(SendTo::RelaySameMessage(downstream)) => {
-                let sv2_frame: codec_sv2::Sv2Frame<MiningDeviceMessages, Vec<u8>> =
+                let sv2_frame: codec_sv2::Sv2Frame<MiningDeviceMessages, buffer_sv2::Slice> =
                     incoming.map(|payload| payload.try_into().unwrap());
 
                 DownstreamMiningNode::send(downstream.clone(), sv2_frame)
@@ -307,7 +307,7 @@ impl UpstreamMiningNode {
                                 .unwrap();
                         }
                         SendTo::RelaySameMessage(downstream_mutex) => {
-                            let frame: codec_sv2::Sv2Frame<MiningDeviceMessages, Vec<u8>> =
+                            let frame: codec_sv2::Sv2Frame<MiningDeviceMessages, buffer_sv2::Slice> =
                                 incoming.clone().map(|payload| payload.try_into().unwrap());
                             DownstreamMiningNode::send(downstream_mutex, frame)
                                 .await
