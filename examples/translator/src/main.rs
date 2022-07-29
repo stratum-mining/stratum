3fac524e8fa3227b786f4c353f5eb675607e7adc
RJ Rybarczyk <rj@rybar.tech>
2022-07-29 17:31:40 -0400

    Add SetupConnectionHandler + impl ParseDownstreamCommonMessages + ParseUpstreamCommonMessages

--- examples/translator/src/main.rs
+++ examples/translator/src/main.rs
@@ -1,11 +1,20 @@
 use async_channel::{Receiver, Sender};
 use codec_sv2::StandardEitherFrame;
 use network_helpers::plain_connection_tokio::PlainConnection;
-use roles_logic_sv2::parsers::MiningDeviceMessages;
+use roles_logic_sv2::{
+    common_messages_sv2::{SetupConnection, SetupConnectionSuccess},
+    common_properties::CommonDownstreamData,
+    errors::Error,
+    handlers::common::{ParseDownstreamCommonMessages, ParseUpstreamCommonMessages},
+    parsers::{CommonMessages, MiningDeviceMessages},
+    routing_logic::NoRouting,
+    utils::Mutex,
+};
 use serde::Deserialize;
 use std::{
     net::{IpAddr, SocketAddr},
     str::FromStr,
+    sync::Arc,
 };
 use tokio::net::{TcpListener, TcpStream};
 
@@ -31,6 +40,71 @@ pub struct Config {
     listen_mining_port: u16,
     max_supported_version: u16,
     min_supported_version: u16,
+}
+
+/// Handles the opening connections:
+/// 1. Downstream (Mining Device) <-> Upstream Proxy
+/// 2. Downstream Proxy <-> Upstream Pool
+struct SetupConnectionHandler {}
+
+/// Implement the `ParseUpstreamCommonMessages` trait for `SetupConnectionHandler`.
+impl ParseUpstreamCommonMessages<NoRouting> for SetupConnectionHandler {
+    /// Upstream sends the Downstream (this proxy) back a `SetupConnection.Success` message on a
+    /// successful connection setup. This functions handles that response.
+    fn handle_setup_connection_success(
+        &mut self,
+        _: SetupConnectionSuccess,
+    ) -> Result<roles_logic_sv2::handlers::common::SendTo, roles_logic_sv2::errors::Error> {
+        use roles_logic_sv2::handlers::common::SendTo;
+        Ok(SendTo::None(None))
+    }
+
+    /// Upstream sends the Downstream (this proxy) back a `SetupConnection.Error` message on an
+    /// unsuccessful connection setup. This functions handles that response.
+    fn handle_setup_connection_error(
+        &mut self,
+        _: roles_logic_sv2::common_messages_sv2::SetupConnectionError,
+    ) -> Result<roles_logic_sv2::handlers::common::SendTo, roles_logic_sv2::errors::Error> {
+        todo!()
+    }
+
+    fn handle_channel_endpoint_changed(
+        &mut self,
+        _: roles_logic_sv2::common_messages_sv2::ChannelEndpointChanged,
+    ) -> Result<roles_logic_sv2::handlers::common::SendTo, roles_logic_sv2::errors::Error> {
+        todo!()
+    }
+}
+
+impl ParseDownstreamCommonMessages<NoRouting> for SetupConnectionHandler {
+    fn handle_setup_connection(
+        &mut self,
+        incoming: SetupConnection,
+        _: Option<Result<(CommonDownstreamData, SetupConnectionSuccess), Error>>,
+    ) -> Result<roles_logic_sv2::handlers::common::SendTo, Error> {
+        use roles_logic_sv2::handlers::common::SendTo;
+        let header_only = incoming.requires_standard_job();
+        // self.header_only = Some(header_only);
+        Ok(SendTo::RelayNewMessage(
+            Arc::new(Mutex::new(())),
+            CommonMessages::SetupConnectionSuccess(SetupConnectionSuccess {
+                flags: 0,
+                used_version: 2,
+            }),
+        ))
+    }
+}
+
+/// A 1 to 1 connection with an upstream node that implements the mining (sub)protocol.
+/// The upstream node it connects with is most typically a pool, but could also be another proxy.
+pub struct Upstream {
+    receiver: Receiver<EitherFrame>,
+    sender: Sender<EitherFrame>,
+}
+
+pub struct Downstream {
+    receiver: Receiver<EitherFrame>,
+    sender: Sender<EitherFrame>,
 }
 
 /// Sv1 Upstream (Miner) <-> Sv1/Sv2 Proxy <-> Sv2 Upstream (Pool)
