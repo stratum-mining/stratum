7b664b14551d17c87852fac99c51a5ac75e4261f
fi3 <email@email.org>
2022-08-02 15:02:47 +0200

    Refactor translator modules

--- examples/translator/src/downstream/mod.rs
+++ examples/translator/src/downstream/mod.rs
@@ -1,53 +1,115 @@
-use async_channel::{Receiver, Sender};
-use codec_sv2::{Frame, StandardEitherFrame, StandardSv2Frame};
-use network_helpers::plain_connection_tokio::PlainConnection;
-use roles_logic_sv2::{
-    handlers::common::{ParseDownstreamCommonMessages, SendTo as SendToCommon},
-    parsers::PoolMessages,
-    utils::Mutex,
+use async_std::net::{TcpListener, TcpStream};
+use std::convert::TryInto;
+
+use async_channel::{bounded, Receiver, Sender};
+use async_std::{
+    io::BufReader,
+    prelude::*,
+    sync::{Arc, Mutex},
+    task,
 };
-use std::{net::SocketAddr, sync::Arc};
-use tokio::{net::TcpListener, task};
+use std::time;
+use v1::{
+    client_to_server,
+    error::Error,
+    json_rpc, server_to_client,
+    utils::{self, HexBytes, HexU32Be},
+    ClientStatus, IsClient, IsServer,
+};
 
-pub mod downstream_mining_node;
-pub mod downstream_mining_node_status;
-pub use downstream_mining_node::DownstreamMiningNode;
+pub async fn listen_downstream() {
+    let listner = TcpListener::bind(crate::LISTEN_ADDR).await.unwrap();
+    let mut incoming = listner.incoming();
+    while let Some(stream) = incoming.next().await {
+        let stream = stream.unwrap();
+        println!("SERVER - Accepting from: {}", stream.peer_addr().unwrap());
+        let server = Downstream::new(stream).await;
+        Arc::new(Mutex::new(server));
+    }
+}
 
-pub type Message = PoolMessages<'static>;
-pub type StdFrame = StandardSv2Frame<Message>;
-pub type EitherFrame = StandardEitherFrame<Message>;
+struct Downstream {
+    receiver_incoming: Receiver<String>,
+    sender_outgoing: Sender<String>,
+}
 
-pub async fn listen_for_downstream_mining(address: SocketAddr) {
-    let listner = TcpListener::bind(address).await.unwrap();
+impl Downstream {
+    pub async fn new(stream: TcpStream) -> Arc<Mutex<Self>> {
+        let stream = std::sync::Arc::new(stream);
 
-    while let Ok((stream, _)) = listner.accept().await {
-        let (receiver, sender): (Receiver<EitherFrame>, Sender<EitherFrame>) =
-            PlainConnection::new(stream).await;
-        let node = DownstreamMiningNode::new(receiver, sender);
+        let (reader, writer) = (stream.clone(), stream);
+
+        let (sender_incoming, receiver_incoming) = bounded(10);
+        let (sender_outgoing, receiver_outgoing) = bounded(10);
 
         task::spawn(async move {
-            let mut incoming: StdFrame = node.receiver.recv().await.unwrap().try_into().unwrap();
-            let message_type = incoming.get_header().unwrap().msg_type();
-            let payload = incoming.payload();
-            let routing_logic = crate::get_common_routing_logic();
-            let node = Arc::new(Mutex::new(node));
-
-            // Call handle_setup_connection or fail
-            match DownstreamMiningNode::handle_message_common(
-                node.clone(),
-                message_type,
-                payload,
-                routing_logic,
-            ) {
-                Ok(SendToCommon::RelayNewMessage(_, message)) => {
-                    let message = match message {
-                        roles_logic_sv2::parsers::CommonMessages::SetupConnectionSuccess(m) => m,
-                        _ => panic!(),
-                    };
-                    DownstreamMiningNode::start(node, message).await
-                }
-                _ => panic!(),
+            let mut messages = BufReader::new(&*reader).lines();
+            while let Some(message) = messages.next().await {
+                let message = message.unwrap();
+                println!("{}", message);
+                sender_incoming.send(message).await.unwrap();
             }
         });
+
+        task::spawn(async move {
+            loop {
+                let message: String = receiver_outgoing.recv().await.unwrap();
+                (&*writer).write_all(message.as_bytes()).await.unwrap();
+            }
+        });
+
+        let dowstream = Arc::new(Mutex::new(Downstream {
+            receiver_incoming,
+            sender_outgoing,
+        }));
+
+        let cloned = dowstream.clone();
+
+        task::spawn(async move {
+            loop {
+                if let Some(mut self_) = cloned.try_lock() {
+                    let incoming = self_.receiver_incoming.try_recv();
+                    self_.parse_message(incoming).await;
+                    drop(self_);
+                };
+            }
+        });
+
+        let cloned = dowstream.clone();
+        task::spawn(async move {
+            loop {
+                if let Some(mut self_) = cloned.try_lock() {
+                    let outgoing = self_.sender_outgoing.send(String::from("hello")).await;
+                    // self_.send_notify().await;
+                    // drop(self_);
+                    task::sleep(time::Duration::from_secs(5)).await;
+                };
+            }
+        });
+
+        dowstream
+    }
+
+    #[allow(clippy::single_match)]
+    async fn parse_message(
+        &mut self,
+        incoming_message: Result<String, async_channel::TryRecvError>,
+    ) {
+        if let Ok(line) = incoming_message {
+            println!("SERVER - message: {}", line);
+            let message: Result<json_rpc::Message, _> = serde_json::from_str(&line);
+            match message {
+                Ok(message) => {
+                    self.send_message().await;
+                }
+                Err(_) => (),
+            }
+        };
+    }
+
+    async fn send_message(&mut self) {
+        // let msg = format!("{}\n", serde_json::to_string(&msg).unwrap());
+        let msg = String::from("HI");
+        self.sender_outgoing.send(msg).await.unwrap();
     }
 }
