0ff1471d115a573c6a574ef2832d8e6cf627fac8
Rachel Rybarczyk <rj@rybar.tech>
2022-08-04 18:14:09 +0000

    Rm unused imports + use pub crate

--- examples/translator/src/upstream_sv2/mod.rs
+++ examples/translator/src/upstream_sv2/mod.rs
@@ -1,4 +1,3 @@
-use crate::downstream_sv1::Downstream;
 use codec_sv2::{StandardEitherFrame, StandardSv2Frame};
 use roles_logic_sv2::parsers::PoolMessages;
 
@@ -7,9 +6,9 @@ pub mod upstream_connection;
 pub(crate) use upstream::Upstream;
 pub(crate) use upstream_connection::UpstreamConnection;
 
-pub type Message = PoolMessages<'static>;
-pub type StdFrame = StandardSv2Frame<Message>;
-pub type EitherFrame = StandardEitherFrame<Message>;
+pub(crate) type Message = PoolMessages<'static>;
+pub(crate) type StdFrame = StandardSv2Frame<Message>;
+pub(crate) type EitherFrame = StandardEitherFrame<Message>;
 
 #[derive(Clone, Copy, Debug)]
 pub struct Sv2MiningConnection {
