61bb2f83b61e5f6e7fa255f40503edc69a9d4eaf
Rachel Rybarczyk <rj@rybar.tech>
2022-08-16 23:10:05 +0000

    Add Err variants + fmt display

--- protocols/v2/codec-sv2/src/error.rs
+++ protocols/v2/codec-sv2/src/error.rs
@@ -1,7 +1,13 @@
+use core::fmt;
+
 #[repr(C)]
 #[derive(Debug)]
 pub enum Error {
+    /// Error if Noise protocol state should already be initialized
+    ExpectedToBeInitialized,
+    /// Errors if there are missing bytes in the Noise protocol
     MissingBytes(usize),
+    /// Catch all
     Todo,
 }
 
@@ -13,8 +19,15 @@ impl From<()> for Error {
     }
 }
 
-impl core::fmt::Display for Error {
-    fn fmt(&self, _: &mut core::fmt::Formatter<'_>) -> core::result::Result<(), core::fmt::Error> {
-        Ok(())
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Error::*;
+        match self {
+            ExpectedToBeInitialized => {
+                write!(f, "Expected Noise state to be already initialized")
+            }
+            MissingBytes(u) => write!(f, "Missing `{}` Noise bytes", u),
+            Todo => write!(f, "Codec Sv2 Error: TODO"),
+        }
     }
 }
