c1227a406b45cdfd47c3e005d5b43d590f74c0d4
Rachel Rybarczyk <rj@rybar.tech>
2022-08-17 17:41:03 +0000

    Declare noise_sv2 feature use to fix codec compilation errs

--- protocols/v2/codec-sv2/src/error.rs
+++ protocols/v2/codec-sv2/src/error.rs
@@ -1,14 +1,16 @@
 use core::fmt;
+#[cfg(feature = "noise_sv2")]
+use noise_sv2::Error as NoiseError;
 
 #[repr(C)]
-#[cfg(feature = "noise_sv2")]
 #[derive(Debug)]
 pub enum Error {
     /// Error if Noise protocol state is not as expected
     UnexpectedNoiseState,
     /// Errors if there are missing bytes in the Noise protocol
     MissingBytes(usize),
-    NoiseSv2Error(noise_sv2::Error),
+    #[cfg(feature = "noise_sv2")]
+    NoiseSv2Error(NoiseError),
     CodecTodo,
     /// Catch all
     CodecCatchAll,
@@ -24,6 +26,7 @@ impl fmt::Display for Error {
                 write!(f, "Noise state is incorrect")
             }
             MissingBytes(u) => write!(f, "Missing `{}` Noise bytes", u),
+            #[cfg(feature = "noise_sv2")]
             NoiseSv2Error(e) => write!(f, "Noise SV2 Error: `{:?}`", e),
             CodecTodo => write!(f, "Codec Sv2 Error: TODO"),
             CodecCatchAll => write!(f, "Codec Sv2 Error: CATCH ALL"),
@@ -37,8 +40,9 @@ impl From<()> for Error {
     }
 }
 
-impl From<noise_sv2::Error> for Error {
-    fn from(e: noise_sv2::Error) -> Self {
+#[cfg(feature = "noise_sv2")]
+impl From<NoiseError> for Error {
+    fn from(e: NoiseError) -> Self {
         Error::NoiseSv2Error(e)
     }
 }
