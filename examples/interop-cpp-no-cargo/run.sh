7e85eebb03470871326ac6ed64fc189c4b0e270d
dcoleman <dcoleman@foundrydigital.com>
2022-09-12 12:40:44 -0400

    71 - add another sleep so provider ist listening

--- examples/interop-cpp-no-cargo/run.sh
+++ examples/interop-cpp-no-cargo/run.sh
@@ -12,5 +12,23 @@ rm -f sv2.h
 
 g++ -I ../../protocols/v2/sv2-ffi ../interop-cpp/template-provider/template-provider.cpp  libsv2_ffi.a  -lpthread -ldl
 
+./a.out &
+provider_pid=$!
+sleep 1 # wait for provider to start listening
 cargo run --manifest-path ../interop-cpp/Cargo.toml &
-./a.out
+run_pid=$!
+
+# If there is a first argument sleep for that long
+if [ -n "$1" ]; then
+    sleep "$1"
+
+  if ps -p $provider_pid > /dev/null && ps -p $run_pid > /dev/null
+  then
+      echo "Success!"
+      kill $provider_pid
+      kill $run_pid
+  else
+      echo "Failure!!!"
+      exit 1
+  fi
+fi
