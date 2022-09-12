0d34973baf60ee368be4f42b512dd72a9b014fe5
Rachel Rybarczyk <rj@rybar.tech>
2022-09-12 16:45:09 +0000

    Handle Notify returns error now

--- examples/sv1-client-and-server/src/main.rs
+++ examples/sv1-client-and-server/src/main.rs
@@ -253,7 +253,7 @@ impl IsServer for Server {
         self.version_rolling_min_bit = mask
     }
 
-    fn notify(&mut self) -> Result<json_rpc::Message, ()> {
+    fn notify(&mut self) -> Result<json_rpc::Message, Error> {
         server_to_client::Notify {
             job_id: "ciao".to_string(),
             prev_hash: utils::PrevHash(vec![3_u8, 4, 5, 6]),
