9bf124ae4a6a7d42b172f8dc74274e5ccbb31ef3
dcoleman <dcoleman@foundrydigital.com>
2022-09-16 10:20:50 -0400

    71 - removing all debugging logging

--- examples/interop-cpp-no-cargo/deps/const_sv2.d
+++
@@ -1,7 +0,0 @@
-./deps/const_sv2.rmeta: ../../protocols/v2//const-sv2/src/lib.rs
-
-./deps/libconst_sv2.rlib: ../../protocols/v2//const-sv2/src/lib.rs
-
-./deps/const_sv2.d: ../../protocols/v2//const-sv2/src/lib.rs
-
-../../protocols/v2//const-sv2/src/lib.rs:
--- examples/interop-cpp-no-cargo/deps/derive_codec_sv2.d
+++
@@ -1,5 +0,0 @@
-./deps/libderive_codec_sv2.so: ../../protocols/v2//binary-sv2/no-serde-sv2/derive_codec/src/lib.rs
-
-./deps/derive_codec_sv2.d: ../../protocols/v2//binary-sv2/no-serde-sv2/derive_codec/src/lib.rs
-
-../../protocols/v2//binary-sv2/no-serde-sv2/derive_codec/src/lib.rs:
