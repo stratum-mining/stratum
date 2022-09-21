1159a210779ab12c0f83fb27362142e29db82558
dcoleman <dcoleman@foundrydigital.com>
2022-09-21 13:17:59 -0400

    This works locally - maybe not in CI - unfortunately I can't test it before pushing to main.

--- utils/buffer/fuzz/run.sh
+++ utils/buffer/fuzz/run.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 set -ex
 
+rustup toolchain install nightly
 cargo +nightly install cargo-fuzz
 cargo +nightly --version
 cargo +nightly fuzz run faster -- -rss_limit_mb=5000000000 -runs=1000
