4ec1b85ff8c3a832b0b453bf83c761a228cdc654
Rachel Rybarczyk <rj@rybar.tech>
2022-09-28 21:29:57 +0000

    new_extranonce_size fn doc cmts

--- roles/translator/src/upstream_sv2/mod.rs
+++ roles/translator/src/upstream_sv2/mod.rs
@@ -18,6 +18,11 @@ pub struct Sv2MiningConnection {
     setup_connection_success_flags: u32,
 }
 
+/// Set the minimum `extranonce_size` to be requested from the Upstream. Request is sent in the
+/// SV2 `OpenExtendedMiningChannel` message. The SV2 Upstream sends the final `extranonce_size` to
+/// the `Bridge` in the SV2 `OpenExtendedMiningChannelSuccess` message response. This
+/// `extranonce_size` is the `extranonce2_size` that is sent to the Downstream in the SV1
+/// `mining.subscribe` message response.
 pub fn new_extranonce2_size() -> u16 {
     8
 }
