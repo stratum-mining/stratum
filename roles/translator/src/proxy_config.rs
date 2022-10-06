9953ed3e7d63afd0a1b173e447b27a5c71e695c9
ccdle12 <chris.coverdale24@gmail.com>
2022-10-06 18:54:18 +0000

    translator: read and deserialize base58 auth key str from proxy config

--- roles/translator/src/proxy_config.rs
+++ roles/translator/src/proxy_config.rs
@@ -1,10 +1,11 @@
 use serde::Deserialize;
+use codec_sv2::noise_sv2::formats::EncodedEd25519PublicKey;
 
 #[derive(Debug, Deserialize)]
 pub struct ProxyConfig {
     pub upstream_address: String,
     pub upstream_port: u16,
-    pub upstream_authority_pubkey: [u8; 32],
+    pub upstream_authority_pubkey: EncodedEd25519PublicKey,
     pub downstream_address: String,
     pub downstream_port: u16,
     pub max_supported_version: u16,
