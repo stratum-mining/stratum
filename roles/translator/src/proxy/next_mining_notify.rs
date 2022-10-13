8cf3dddac4b6da5752ffd470299a2f2ecb0131c0
Rachel Rybarczyk <rj@rybar.tech>
2022-10-13 20:46:55 +0000

    NextMiningNotify doc cmts

--- roles/translator/src/proxy/next_mining_notify.rs
+++ roles/translator/src/proxy/next_mining_notify.rs
@@ -6,11 +6,17 @@ use v1::{
     utils::{HexBytes, HexU32Be, PrevHash},
 };
 
-/// Given a new SV2 `SetNewPrevHash` and/or `NewExtendedMiningJob` message, creates a new SV1
-/// `mining.notify` message.
+/// To create a SV1 `mining.notify` message, both a SV2 `SetNewPrevHash` and `NewExtendedMiningJob`
+/// message must be received. These messages are sent by the Upstream role at different times, so
+/// this struct serves as the means to store each message as they are received. It also performs
+/// the translation into the SV1 `mining.notify` message.
 #[derive(Clone, Debug)]
 pub struct NextMiningNotify {
+    /// Stores the SV2 `SetNewPrevHash` message to be later translated (along with the SV2
+    /// `NewExtendedMiningJob` message) to a SV1 `mining.notify` message.
     pub set_new_prev_hash: Option<SetNewPrevHash<'static>>,
+    /// Stores the SV2 `NewExtendedMiningJob` message to be later translated (along with the SV2
+    /// `SetNewPrevHash` message) to a SV1 `mining.notify` message.
     pub new_extended_mining_job: Option<NewExtendedMiningJob<'static>>,
 }
 
@@ -23,14 +29,14 @@ impl NextMiningNotify {
         }
     }
 
-    /// Sets `set_new_prev_hash` member field upon `Bridge` receiving a SV2 `SetNewPrevHash`
+    /// Sets `set_new_prev_hash` member field upon the `Bridge` receiving a SV2 `SetNewPrevHash`
     /// message from `Upstream`. Used in conjunction with `NewExtendedMiningJob` to create a SV1
     /// `mining.notify` message.
     pub(crate) fn set_new_prev_hash_msg(&mut self, set_new_prev_hash: SetNewPrevHash<'static>) {
         self.set_new_prev_hash = Some(set_new_prev_hash);
     }
 
-    /// Sets `new_extended_mining_job` member field upon `Bridge` receiving a SV2
+    /// Sets `new_extended_mining_job` member field upon the `Bridge` receiving a SV2
     /// `NewExtendedMiningJob` message from `Upstream`. Used in conjunction with `SetNewPrevHash`
     /// to create a SV1 `mining.notify` message.
     pub(crate) fn new_extended_mining_job_msg(
@@ -40,34 +46,42 @@ impl NextMiningNotify {
         self.new_extended_mining_job = Some(new_extended_mining_job);
     }
 
-    /// Creates a new SV1 `mining.notify` message on a new SV2 `SetNewPrevHash` and/or new
-    /// `NewExtendedMiningJob` message.
+    /// Creates a new SV1 `mining.notify` message if both SV2 `SetNewPrevHash` and
+    /// `NewExtendedMiningJob` messages have been received. If one of these messages is still being
+    /// waited on, the function returns `None`.
     pub(crate) fn create_notify(&self) -> Option<server_to_client::Notify> {
-        // Put logic in to make sure that SetNewPrevHash + NewExtendedMiningJob is matching (not
-        // future)
-        // if new_prev_hash.job_id != new_job.job_id {
-        //     panic!("TODO: SetNewPrevHash + NewExtendedMiningJob job id's do not match");
-        // }
-        //
+        // Make sure that SetNewPrevHash + NewExtendedMiningJob is matching (not future)
+        if self.set_new_prev_hash.is_some() && self.new_extended_mining_job.is_some() {
+            let set_new_prev_hash_job_id = &self.set_new_prev_hash.as_ref().unwrap().job_id;
+            let new_extended_mining_job_job_id =
+                &self.new_extended_mining_job.as_ref().unwrap().job_id;
+            if set_new_prev_hash_job_id != new_extended_mining_job_job_id {
+                panic!(
+                    "Job Id Mismatch. SetNewPrevHash Job Id: {}, NewExtendedMiningJob Job Id: {}",
+                    set_new_prev_hash_job_id, new_extended_mining_job_job_id
+                );
+            }
+        }
+
         match (&self.set_new_prev_hash, &self.new_extended_mining_job) {
-            (Some(new_prev_hash),Some(new_job)) => {
+            // If both the SV2 `SetNewPrevHash` and `NewExtendedMiningJob` messages are present,
+            // translate into a SV1 `mining.notify` message
+            (Some(new_prev_hash), Some(new_job)) => {
                 let job_id = new_prev_hash.job_id.to_string();
 
-                // TODO: Check endianness
                 // U256<'static> -> PrevHash
-                let mut prev_hash = new_prev_hash.prev_hash.clone().to_vec();
+                let prev_hash = new_prev_hash.prev_hash.clone().to_vec();
                 let prev_hash = PrevHash(prev_hash);
 
-                // TODO: Check endianness
                 // B064K<'static'> -> HexBytes
                 let coin_base1 = new_job.coinbase_tx_prefix.to_vec().into();
                 let coin_base2 = new_job.coinbase_tx_suffix.to_vec().into();
 
                 // Seq0255<'static, U56<'static>> -> Vec<Vec<u8>> -> Vec<HexBytes>
                 let merkle_path = new_job.merkle_path.clone().into_static().to_vec();
-                let merkle_branch: Vec<HexBytes> = merkle_path.into_iter().map(|p| p.into()).collect();
+                let merkle_branch: Vec<HexBytes> =
+                    merkle_path.into_iter().map(|p| p.into()).collect();
 
-                // TODO: Check endianness
                 // u32 -> HexBytes
                 let version = HexU32Be(new_job.version);
                 let bits = HexU32Be(new_prev_hash.nbits);
@@ -87,7 +101,9 @@ impl NextMiningNotify {
                     clean_jobs,
                 };
                 Some(notify_response)
-            },
+            }
+            // If either of the SV2 `SetNewPrevHash` or `NewExtendedMiningJob` have not been
+            // received, cannot make a SV1 `mining.notify` message so return `None`
             _ => None,
         }
     }
