38e88aa2214ed7acc87deffdd5afa3d1d80755e5
dcoleman <dcoleman@foundrydigital.com>
2022-10-20 16:48:51 -0400

    207 - fmt

--- roles/translator/src/main.rs
+++ roles/translator/src/main.rs
@@ -23,7 +23,6 @@ use v1::server_to_client;
 use tracing_subscriber;
 
 use tracing::{error, info};
-
 /// Process CLI args, if any.
 fn process_cli_args() -> ProxyResult<ProxyConfig> {
     let args = match Args::from_args() {
