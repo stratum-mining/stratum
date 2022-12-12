cbd48ab56adc6e6a52f8341cf21ed5673a4a295f
Cole Roberts <croberts@foundrydigital.com>
2022-12-12 10:05:32 -0500

    remove hex crate from sv1-client-and-server

--- examples/sv1-client-and-server/src/main.rs
+++ examples/sv1-client-and-server/src/main.rs
@@ -26,12 +26,12 @@ fn new_extranonce<'a>() -> Extranonce<'a> {
 }
 
 fn extranonce_from_hex<'a>(hex: &str) -> Extranonce<'a> {
-    let data = hex::decode(hex).unwrap();
+    let data = utils::decode_hex(hex).unwrap();
     Extranonce::try_from(data).expect("Failed to convert hex to U256")
 }
 
 fn merklenode_from_hex<'a>(hex: &str) -> MerkleNode<'a> {
-    let data = hex::decode(hex).unwrap();
+    let data = utils::decode_hex(hex).unwrap();
     let len = data.len();
     if hex.len() >= 64 {
         // panic if hex is larger than 32 bytes
@@ -40,12 +40,13 @@ fn merklenode_from_hex<'a>(hex: &str) -> MerkleNode<'a> {
         // prepend hex with zeros so that it is 32 bytes
         let mut new_vec = vec![0_u8; 32 - len];
         new_vec.extend(data.iter());
-        MerkleNode::try_from(hex::encode(new_vec).as_str()).expect("Failed to convert hex to U256")
+        MerkleNode::try_from(utils::encode_hex(&new_vec).as_str())
+            .expect("Failed to convert hex to U256")
     }
 }
 
 fn prevhash_from_hex<'a>(hex: &str) -> PrevHash<'a> {
-    let data = hex::decode(hex).unwrap();
+    let data = utils::decode_hex(hex).unwrap();
     let len = data.len();
     if hex.len() >= 64 {
         // panic if hex is larger than 32 bytes
@@ -54,7 +55,8 @@ fn prevhash_from_hex<'a>(hex: &str) -> PrevHash<'a> {
         // prepend hex with zeros so that it is 32 bytes
         let mut new_vec = vec![0_u8; 32 - len];
         new_vec.extend(data.iter());
-        PrevHash::try_from(hex::encode(new_vec).as_str()).expect("Failed to convert hex to U256")
+        PrevHash::try_from(utils::encode_hex(&new_vec).as_str())
+            .expect("Failed to convert hex to U256")
     }
 }
 
@@ -631,4 +633,54 @@ fn main() {
         let client = Client::new(80, socket).await;
         initialize_client(client).await;
     });
+}
+
+mod utils {
+    use std::fmt::Write;
+
+    pub fn decode_hex(s: &str) -> Result<Vec<u8>, core::num::ParseIntError> {
+        let s = match s.strip_prefix("0x") {
+            Some(s) => s,
+            None => s,
+        };
+        (0..s.len())
+            .step_by(2)
+            .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+            .collect()
+    }
+
+    pub fn encode_hex(bytes: &[u8]) -> String {
+        let mut s = String::with_capacity(bytes.len() * 2);
+        for &b in bytes {
+            write!(&mut s, "{:02x}", b).unwrap();
+        }
+        s
+    }
+
+    #[cfg(test)]
+    #[test]
+    fn test() {
+        let test_vec = vec![222, 173, 190, 239];
+        let hex_og = "deadbeef";
+        let hex_og_w_prefix = "0xdeadbeef";
+        // decode hex strings
+        let decoded = decode_hex(hex_og).unwrap();
+        let decoded_w_prefix = decode_hex(hex_og_w_prefix).unwrap();
+
+        assert_eq!(&decoded, &test_vec, "Hex not decoded correctly");
+        assert_eq!(
+            &decoded_w_prefix, &test_vec,
+            "Hex w/ prefix not decoded correctly"
+        );
+
+        // reencode
+        let reencoded = encode_hex(&decoded);
+        let reencoded_prefix = encode_hex(&decoded);
+
+        assert_eq!(&reencoded, hex_og, "Hex not encoded correctly");
+        assert_eq!(
+            &reencoded_prefix, &hex_og,
+            "Hex w/ prefix not encoded correctly"
+        );
+    }
 }
