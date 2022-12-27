ac717e9bff4caaa3a88fd4520980c02e2c1a83bc
fi3 <email@email.org>
2022-12-27 17:01:27 +0100

    Fix prop test build

--- protocols/v2/subprotocols/mining/src/lib.rs
+++ protocols/v2/subprotocols/mining/src/lib.rs
@@ -1054,7 +1054,7 @@ pub mod tests {
     }
 
     #[quickcheck_macros::quickcheck]
-    fn test_vec_from_extranonce(mut input: Vec<u8>) -> bool {
+    fn test_vec_from_extranonce(input: Vec<u8>) -> bool {
         let input_start = from_arbitrary_vec_to_array(input).to_vec();
         let extranonce_start = Extranonce::try_from(input_start.clone()).unwrap();
         let vec_final = Vec::from(extranonce_start.clone());
