532671ab346757b12990c2aaaec5b960e17ceebe
lorban <lorenzo.bonax@gmail.com>
2023-02-23 11:41:50 +0100

    Make paths more intuitive in tests
    Now it is possible to launch a test from the MG directory by using
    simply "cargo run [relative path of the test], without the additional
    "../". Moreover, in the frame builders relative to the common messages,
    it is possible to specify the frame with the following pattern
    [path-of-the-module]::[framebuilder].
    For example, if we want to write a test that setis a standard channel with
    the pool for HOM mining, the message-id of the frame builer is
    test/message-generator/messages/common_messages.json::setup_connection_mining_hom

--- utils/message-generator/src/parser/frames.rs
+++ utils/message-generator/src/parser/frames.rs
@@ -39,6 +39,9 @@ impl<'a> Frames<'a> {
                 2 => {
                     /// the function "message_from_id" returns a an AnyMessage from the path in
                     /// input
+                    let mut path = id[0].clone();
+                    std::string::String::insert_str(&mut path, 0, "../../../../");
+                    let id = vec![path, id[1].clone()];
                     let message = message_from_path(&id);
                     let id = id[1].clone();
                     (message, id)
