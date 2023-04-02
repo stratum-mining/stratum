30e54e71ae6dd7269bf4f18a4596b01bd7f81bed
ElRo <elia.ro91@gmail.com>
2023-04-02 21:18:10 +0200

    start issue #500 job_ids

--- roles/translator/src/upstream_sv2/upstream.rs
+++ roles/translator/src/upstream_sv2/upstream.rs
@@ -82,6 +82,8 @@ pub struct Upstream {
     pub(super) channel_id: Option<u32>,
     /// Identifier of the job as provided by the `NewExtendedMiningJob` message.
     job_id: Option<u32>,
+    /// Identifier of the job as provided by the ` SetCustomMiningJobSucces` message
+    last_upstream_job_id: Option<u32>,
     /// Bytes used as implicit first part of `extranonce`.
     extranonce_prefix: Option<Vec<u8>>,
     /// Represents a connection to a SV2 Upstream role.
@@ -183,6 +185,7 @@ impl Upstream {
             tx_sv2_new_ext_mining_job,
             channel_id: None,
             job_id: None,
+            last_upstream_job_id: None,
             min_extranonce_size,
             upstream_extranonce1_size: 16, // 16 is the default since that is the only value the pool supports currently
             tx_sv2_extranonce,
@@ -481,8 +484,10 @@ impl Upstream {
         self_
             .safe_lock(|s| {
                 if s.is_work_selection_enabled() {
-                    // TODO MVP3 this will be set by SetCustomMiningJobSuccess
-                    Ok(0)
+                    info!("BBBBBB last_upstream_job_id in get_job_id function {:?}", {s.last_upstream_job_id.unwrap()});
+                    s.last_upstream_job_id.ok_or(crate::error::Error::RolesSv2Logic(
+                        RolesLogicError::NoValidJob,
+                    ))                    
                 } else {
                     s.job_id.ok_or(crate::error::Error::RolesSv2Logic(
                         RolesLogicError::NoValidJob,
@@ -816,8 +821,10 @@ impl ParseUpstreamMiningMessages<Downstream, NullDownstreamMiningSelector, NoRou
     /// Handles the SV2 `SetCustomMiningJobSuccess` message (TODO).
     fn handle_set_custom_mining_job_success(
         &mut self,
-        _m: roles_logic_sv2::mining_sv2::SetCustomMiningJobSuccess,
+        m: roles_logic_sv2::mining_sv2::SetCustomMiningJobSuccess,
     ) -> Result<roles_logic_sv2::handlers::mining::SendTo<Downstream>, RolesLogicError> {
+        self.last_upstream_job_id = Some(m.job_id);
+        info!("AAAAAA last_upstream_job_id: {:?}", {self.last_upstream_job_id.unwrap()});
         Ok(SendTo::None(None))
     }
 
