816e2392215221819a4fd84f0e960148645f2edc
darricksee <dcoleman@foundrydigital.com>
2023-04-26 11:29:45 -0400

    fmt+clippy

--- roles/translator/src/upstream_sv2/upstream.rs
+++ roles/translator/src/upstream_sv2/upstream.rs
@@ -12,7 +12,6 @@ use binary_sv2::u256_from_int;
 use codec_sv2::{Frame, HandshakeRole, Initiator};
 use error_handling::handle_result;
 use network_helpers::Connection;
-use roles_logic_sv2::Error::NoUpstreamsConnected;
 use roles_logic_sv2::{
     bitcoin::BlockHash,
     common_messages_sv2::{Protocol, SetupConnection},
@@ -30,6 +29,7 @@ use roles_logic_sv2::{
     selectors::NullDownstreamMiningSelector,
     utils::Mutex,
     Error as RolesLogicError,
+    Error::NoUpstreamsConnected,
 };
 use std::{
     net::SocketAddr,
@@ -447,7 +447,7 @@ impl Upstream {
                                 debug!("parse_incoming Mining::SetNewPrevHash msg");
                                 handle_result!(tx_status, tx_sv2_set_new_prev_hash.send(m).await);
                             }
-                            Mining::CloseChannel(m) => {
+                            Mining::CloseChannel(_m) => {
                                 error!("Received Mining::CloseChannel msg from upstream!");
                                 handle_result!(tx_status, Err(NoUpstreamsConnected));
                             }
