e17e8d863388651be5ed7fea30586799f032e875
lobarrel <loribottelli@gmail.com>
2023-07-14 11:24:12 +0200

    sv1_messages refactor

--- utils/message-generator/src/parser/sv1_messages.rs
+++ utils/message-generator/src/parser/sv1_messages.rs
@@ -1,37 +1,25 @@
-use v1::server_to_client::{Notify, SetDifficulty, SetExtranonce, Submit, Subscribe, Authorize};
-use v1::client_to_server::{Subscribe, Authorize, Submit};
 use v1::methods::*;
 
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
 struct Sv1ClientMessage<'a> {
-    #[serde(borrow)]
     message: Sv1ClientMessages<'a>,
     id: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
 struct Sv1ServerMessage<'a> {
-    #[serde(borrow)]
     message: Sv1ServerMessages<'a>,
     id: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
 struct Sv1ServerResponse<'a> {
-    #[serde(borrow)]
     message: Sv1ServerResponses<'a>,
     id: String,
 }
 
 //CLIENT TO SERVER
-#[derive(Clone, Debug, Serialize, Deserialize)]
-#[serde(tag = "type")]
 pub enum Sv1ClientMessages<'a> {
-    #[serde(borrow)]
     Subscribe(client_to_server::Subscribe<'a>),
     Authorize(client_to_server::Authorize),
-    #[serde(borrow)]
     Submit(client_to_server::Submit<'a>),
 }
 
@@ -47,12 +35,9 @@ impl<'a> From<Sv1ClientMessages<'a>> for Client2Server<'a>{
 
 
 //SERVER TO CLIENT
-#[derive(Clone, Debug, Serialize, Deserialize)]
-#[serde(tag = "type")]
 pub enum Sv1ServerMessages<'a> {
-    Notify(server_to_client::Notify),
+    Notify(server_to_client::Notify<'a>),
     SetDifficulty(server_to_client::SetDifficulty),
-    #[serde(borrow)]
     SetExtranonce(server_to_client::SetExtranonce<'a>),
 }
 
@@ -68,8 +53,6 @@ impl<'a> From<Sv1ServerMessages<'a>> for Server2Client<'a> {
 
 
 //SERVER TO CLIENT RESPONSES
-#[derive(Clone, Debug, Serialize, Deserialize)]
-#[serde(tag = "type")]
 pub enum Sv1ServerResponses<'a> {
     Subscribe(server_to_client::Subscribe<'a>),
     Authorize(server_to_client::Authorize),
