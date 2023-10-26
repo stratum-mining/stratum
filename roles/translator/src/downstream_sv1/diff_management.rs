3b88b3cf8daf7e2492ed5abf6b02b21c42fe0be9
fi3 <email@email.org>
2023-10-26 12:17:51 +0200

    Fix tests

--- roles/translator/src/downstream_sv1/diff_management.rs
+++ roles/translator/src/downstream_sv1/diff_management.rs
@@ -300,6 +300,7 @@ mod test {
             shares_per_minute: 1000.0,            // 1000 shares per minute
             submits_since_last_update: 0,
             timestamp_of_last_update: 0, // updated below
+            update_interval: None,
         };
         let upstream_config = UpstreamDifficultyConfig {
             channel_diff_update_interval: 60,
