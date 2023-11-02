5266cb5d65fbba33c833256839e1c2e4d35e1f68
Gabriele Vernetti <gabriele.vernetti@protonmail.com>
2023-11-02 11:40:27 +0100

    clippy fix

--- utils/bip32-key-derivation/src/main.rs
+++ utils/bip32-key-derivation/src/main.rs
@@ -13,9 +13,9 @@ fn main() {
     let slip132_master_pub_key = &args[1];
     let derivation_path = &args[2];
     let bip32_extended_pub_key: ExtendedPubKey =
-        slip132::FromSlip132::from_slip132_str(&slip132_master_pub_key).unwrap();
+        slip132::FromSlip132::from_slip132_str(slip132_master_pub_key).unwrap();
     let child_pub_key =
-        bip32_derivation::derive_child_public_key(&bip32_extended_pub_key, &derivation_path)
+        bip32_derivation::derive_child_public_key(&bip32_extended_pub_key, derivation_path)
             .unwrap();
     println!(
         "\nPublic key derived from your Master Public Key -> {:?}",
