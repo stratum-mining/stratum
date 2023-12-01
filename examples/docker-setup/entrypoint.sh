57b6c1a5d30d1f6c56bf38484797ef5d35e41337
Roberto Santacroce <112706229+rsantacroce@users.noreply.github.com>
2023-12-01 18:45:05 +0100

    added docker-compose and entrypoint

--- examples/docker-setup/entrypoint.sh
+++ examples/docker-setup/entrypoint.sh
@@ -1,1 +1,20 @@
-/stratum/target/release/pool_sv2 -c pool-config-local-tp-example.tom
+#!/bin/sh
+
+case "$RUN_COMMAND" in
+  "pool")
+    /stratum/target/release/pool_sv2 -c /conf/pool-config.toml
+    ;;
+  "jd-server")
+    /stratum/target/release/jd_server -c /conf/jds-config.toml
+    ;;
+  "jd-client")
+    /stratum/target/release/jd_server -c /conf/jdc-config.toml
+    ;;
+  "translator")
+    /stratum/target/release/jd_server -c /conf/proxy-config.toml
+    ;;
+  *)
+    echo "Invalid RUN_COMMAND specified"
+    exit 1
+    ;;
+esac
