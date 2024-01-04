e26f3a6296cea1625942ddafc0e1470c9492ffe3
plebhash <plebhash@proton.me>
2024-01-03 21:34:32 -0300

    adapt fmt CI to workspace split

--- protocols/v2/codec-sv2/src/encoder.rs
+++ protocols/v2/codec-sv2/src/encoder.rs
@@ -1,15 +1,12 @@
 use alloc::vec::Vec;
 use binary_sv2::{GetSize, Serialize};
-pub use const_sv2::AEAD_MAC_LEN;
-pub use const_sv2::{SV2_FRAME_CHUNK_SIZE, SV2_FRAME_HEADER_SIZE};
+pub use const_sv2::{AEAD_MAC_LEN, SV2_FRAME_CHUNK_SIZE, SV2_FRAME_HEADER_SIZE};
 #[cfg(feature = "noise_sv2")]
 use core::convert::TryInto;
 use core::marker::PhantomData;
 #[cfg(feature = "noise_sv2")]
 use framing_sv2::framing2::{EitherFrame, HandShakeFrame};
-use framing_sv2::{
-    framing2::{Frame as F_, Sv2Frame},
-};
+use framing_sv2::framing2::{Frame as F_, Sv2Frame};
 pub use framing_sv2::header::NoiseHeader;
 
 #[cfg(feature = "noise_sv2")]
