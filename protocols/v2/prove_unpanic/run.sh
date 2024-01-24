d4aa07f1d7cee2d20e6897452df7a0e5706ce225
fi3 <email@email.org>
2024-01-24 15:49:26 +0100

    Fix clippy

--- protocols/v2/prove_unpanic/run.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-cargo clean && RUST_BACKTRACE=1 RUSTC_WRAPPER=~/src/unpanic/unpanic TARGET_CRATE=prove_unpanic cargo +nightly-2023-08-25-x86_64-unknown-linux-gnu build -p prove_unpanic
