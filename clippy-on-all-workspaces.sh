c67733c94c0f86e833b33494632505f0462364b2
lorban <lorenzo.bonax@gmail.com>
2024-01-29 10:37:07 +0100

    cargo fmt and test + prop test added

--- clippy-on-all-workspaces.sh
+++ clippy-on-all-workspaces.sh
@@ -1,16 +1,30 @@
 #!/bin/sh
 
-WORKSPACES="benches/Cargo.toml common/Cargo.toml protocols/Cargo.toml roles/Cargo.toml
-utils/Cargo.toml"
+WORKSPACES="benches common protocols roles utils"
 
 for workspace in $WORKSPACES; do
     echo "Executing clippy on: $workspace"
-    cargo clippy --manifest-path="$workspace" -- -D warnings -A dead-code
+    cargo clippy --manifest-path="$workspace/Cargo.toml" -- -D warnings -A dead-code
     if [ $? -ne 0 ]; then
         echo "Clippy found some errors in: $workspace"
         exit 1
     fi
+
+    echo "Running tests on: $workspace"
+    cargo test --manifest-path="$workspace/Cargo.toml"
+    if [ $? -ne 0 ]; then
+        echo "Tests failed in: $workspace"
+        exit 1
+    fi
+
+    echo "Running fmt on: $workspace"
+    (cd $workspace && cargo +nightly fmt)
+    if [ $? -ne 0 ]; then
+        echo "Fmt failed in: $workspace"
+        exit 1
+    fi
 done
 
-echo "Clippy success!"
+echo "Clippy success, all tests passed!"
+
 
