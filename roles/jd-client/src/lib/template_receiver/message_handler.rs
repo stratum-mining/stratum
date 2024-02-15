47b929ac4a00e77109eb82d96c082fbd020b54f7
Gabriele Vernetti <gabriele.vernetti@protonmail.com>
2024-02-15 19:16:15 +0100

    string printed in NoValidTemplate error type

--- roles/jd-client/src/lib/template_receiver/message_handler.rs
+++ roles/jd-client/src/lib/template_receiver/message_handler.rs
@@ -47,13 +47,13 @@ impl ParseServerTemplateDistributionMessages for TemplateRx {
             template_id: _m.template_id,
             error_code: _m.error_code.into_static(),
         };
-
-        match std::str::from_utf8(m.error_code.as_ref()).unwrap_or("unknown error code") {
-            "template-id-not-found" => Err(Error::NoValidTemplate),
+        let error_code_string = std::str::from_utf8(m.error_code.as_ref()).unwrap_or("unknown error code"); 
+        match error_code_string {
+            "template-id-not-found" => Err(Error::NoValidTemplate(error_code_string.to_string())),
             "stale-template-id" => Ok(SendTo::None(Some(
                 TemplateDistribution::RequestTransactionDataError(m),
             ))),
-            _ => Err(Error::NoValidTemplate),
+            _ => Err(Error::NoValidTemplate(error_code_string.to_string())),
         }
     }
 }
