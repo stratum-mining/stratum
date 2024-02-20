e69fc53c3df9eecf728b34936a1a75d44a4702e7
lorban <lorenzo.bonax@gmail.com>
2024-02-20 17:03:20 +0100

    easy script for compiling during development
    after that the SRI has been divided in several workspaces for external
    consuption, it is annoying to compile every workspace manually. So, a
    script named clippy-on-all-workspaces.sh has been added. This script is
    mean to aid the the final phase of developing a feature, where build
    compiles and we pass to clippy.
    In a preliminary phase of developing though, it is necessary something
    that compiles a bit less deep and we do want to make tests pass. Thats
    why I propose this script that does exactly this:
    performes `cargo build` and `cargo +nightly fmt` in each workspace.

---
+++ build-on-all-workspaces.sh
@@ -0,0 +1,24 @@
+
+#!/bin/sh
+
+WORKSPACES="benches common protocols roles utils"
+
+for workspace in $WORKSPACES; do
+    echo "Executing build on: $workspace"
+    cargo build --manifest-path="$workspace/Cargo.toml" -- 
+    if [ $? -ne 0 ]; then
+        echo "Build found some errors in: $workspace"
+        exit 1
+    fi
+
+    echo "Running fmt on: $workspace"
+    (cd $workspace && cargo +nightly fmt)
+    if [ $? -ne 0 ]; then
+        echo "Fmt failed in: $workspace"
+        exit 1
+    fi
+done
+
+echo "build success!"
+
+
