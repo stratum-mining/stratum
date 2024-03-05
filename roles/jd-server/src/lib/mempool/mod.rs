60abbf41272828cc190814603a86e91ddef7e302
GitGab19 <gabriele.vernetti@protonmail.com>
2024-03-05 12:23:46 +0100

    fix unclosed bracket

--- roles/jd-server/src/lib/mempool/mod.rs
+++ roles/jd-server/src/lib/mempool/mod.rs
@@ -38,7 +38,7 @@ impl JDsMempool {
     #[cfg(debug_assertions)]
     pub fn _get_transaction_list(self_: Arc<Mutex<Self>>) -> Vec<Txid> {
         let tx_list = self_.safe_lock(|x| x.mempool.clone()).unwrap();
-        let tx_list_: Vec<Txid> = tx_list.iter().map(|n| n.id).collect();
+        let tx_list_: Vec<Txid> = tx_list.iter().map(|n| *n.0).collect();
         tx_list_
     }
     pub fn new(
