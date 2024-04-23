7123a999bea4a9a50efbda8193e5d968df570fe2
lorban <lorenzo.bonax@gmail.com>
2024-04-23 15:33:03 +0200

    Make MG script to build before launching tests
    With this modification, the binaries are built before launching any
    MG-test, making them run quicker and less likely to encounter a timeout.
    This should make the MG-tests a bit more stable

--- message-generator-tests.sh
+++ message-generator-tests.sh
@@ -6,17 +6,33 @@ else
     RUST_LOG="debug"
 fi
 
-search_dir="../../test/message-generator/test/"
 message_generator_dir="./utils/message-generator/"
-
 cd $message_generator_dir
 cargo llvm-cov clean
+
+cd ../../roles
+cargo build -p mining-device
+cargo llvm-cov -p pool_sv2
+cargo llvm-cov -p jd_server
+cargo llvm-cov -p jd_client
+cargo llvm-cov -p translator_sv2 
+cargo llvm-cov -p mining_proxy_sv2 
+cd ./roles-utils
+cargo llvm-cov 
+cd ../../
+
+search_dir="../../test/message-generator/test/"
+
+cd $message_generator_dir
+cargo build
 
 for entry in `ls $search_dir`; do
     if [ "$entry" = "interop-jdc-change-upstream.json" ]; then
         echo "Skipping $entry"
         continue
     fi
+
+    sleep 10
 
     echo $entry
     RUST_LOG=$RUST_LOG cargo run -- $search_dir$entry || { echo 'mg test failed' ; exit 1; }
