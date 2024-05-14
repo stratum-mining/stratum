04db6a05af2416da39dd989ee3385611885a855f
lorban <lorenzo.bonax@gmail.com>
2024-05-14 14:18:23 +0200

    add error print

--- utils/message-generator/src/main.rs
+++ utils/message-generator/src/main.rs
@@ -26,7 +26,7 @@ use std::{
     },
     vec::Vec,
 };
-use tracing::info;
+use tracing::{error, info};
 use tracing_core::{Event, Subscriber};
 use tracing_subscriber::{
     filter::EnvFilter,
@@ -408,7 +408,8 @@ async fn main() {
     let pass = Arc::new(AtomicBool::new(false));
     {
         let fail = fail.clone();
-        std::panic::set_hook(Box::new(move |_| {
+        std::panic::set_hook(Box::new(move |info| {
+            error!("{:#?}", info);
             fail.store(true, Ordering::Relaxed);
         }));
     }
@@ -430,6 +431,7 @@ async fn main() {
         });
     }
     loop {
+        tokio::task::yield_now().await;
         if fail.load(Ordering::Relaxed) {
             clean_up(cleanup).await;
             let _ = std::panic::take_hook();
