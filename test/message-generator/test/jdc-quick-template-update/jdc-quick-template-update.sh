4a81164101e5568af2fb564a89d9dd90e35fb38e
bit-aloo <sshourya17@gmail.com>
2024-07-03 07:56:48 -0400

    Removed redundant code

--- test/message-generator/test/jdc-quick-template-update/jdc-quick-template-update.sh
+++ test/message-generator/test/jdc-quick-template-update/jdc-quick-template-update.sh
@@ -5,6 +5,6 @@ cargo llvm-cov --no-report -p pool_sv2
 cd ../utils/message-generator/
 cargo build
 
-RUST_LOG=debug cargo run ../../test/message-generator/test/jdc-quick-template-update/jdc-quick-template-update.json || { echo 'mg test failed' ; exit 1; }
+RUST_LOG=info cargo run ../../test/message-generator/test/jdc-quick-template-update/jdc-quick-template-update.json || { echo 'mg test failed' ; exit 1; }
 
 sleep 10
