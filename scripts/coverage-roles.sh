3b6e51f2255bffcad0459b75c11d71ac19e7e8bd
bit-aloo <sshourya17@gmail.com>
2024-10-11 21:02:47 +0530

    add roles yaml, coverage-roles and modify roles tarpaulin.toml

---
+++ scripts/coverage-roles.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+tarpaulin() {
+  crate_name=$1
+  output_dir="target/tarpaulin-reports/$crate_name"
+  mkdir -p "$output_dir"
+  cargo +nightly tarpaulin --verbose --out Xml --output-dir "$output_dir" --all-features
+}
+
+cd roles
+tarpaulin
+
+crates=(
+  "mining-proxy"
+  "pool"
+  "test-utils/mining-device"
+  "test-utils/sv1-mining-device"
+  "translator"
+  "jd-client"
+  "jd-server"
+)
+
+for crate in "${crates[@]}"; do
+  echo "Running Tarpaulin for $crate..."
+  crate_name=$(basename "$crate") 
+  cd "$crate" || exit 1            
+  tarpaulin "$crate_name-coverage"
+  cd - || exit 1
+done
