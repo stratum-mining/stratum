60bfee302853577c929a896cd38d033f81a67b9c
jbesraa <jbesraa@gmail.com>
2024-11-08 09:00:33 +0200

    Improve `Template Distribution` subprotocol docs

--- protocols/v2/subprotocols/template-distribution/src/new_template.rs
+++ protocols/v2/subprotocols/template-distribution/src/new_template.rs
@@ -10,51 +10,57 @@ use core::convert::TryInto;
 #[cfg(all(feature = "with_serde", not(feature = "no_std")))]
 use std::convert::TryInto;
 
-/// ## NewTemplate (Server -> Client)
-/// The primary template-providing function. Note that the coinbase_tx_outputs bytes will appear
-/// as is at the end of the coinbase transaction.
+/// Message used by an upstream(Template Provider) to provide a new template for downstream to mine
+/// on.
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 pub struct NewTemplate<'decoder> {
-    /// Server’s identification of the template. Strictly increasing, the
-    /// current UNIX time may be used in place of an ID.
+    /// Upstream’s identification of the template.
+    ///
+    /// Should be strictly increasing.
     pub template_id: u64,
-    /// True if the template is intended for future [`crate::SetNewPrevHash`]
-    /// message sent on the channel. If False, the job relates to the last
-    /// sent [`crate::SetNewPrevHash`] message on the channel and the miner
-    /// should start to work on the job immediately.
+    /// If `True`, the template is intended for future [`crate::SetNewPrevHash`] message sent on
+    /// the channel.
+    ///
+    /// If `False`, the job relates to the last sent [`crate::SetNewPrevHash`] message on the
+    /// channel and the miner should start to work on the job immediately.
     pub future_template: bool,
-    /// Valid header version field that reflects the current network
-    /// consensus. The general purpose bits (as specified in [BIP320]) can
-    /// be freely manipulated by the downstream node. The downstream
-    /// node MUST NOT rely on the upstream node to set the BIP320 bits
-    /// to any particular value.
+    /// Valid header version field that reflects the current network consensus.
+    ///
+    /// The general purpose bits, as specified in
+    /// [BIP320](https://github.com/bitcoin/bips/blob/master/bip-0320.mediawiki), can be freely
+    /// manipulated by the downstream node.
+    ///
+    /// The downstream **must not** rely on the upstream to set the
+    /// [BIP320](https://github.com/bitcoin/bips/blob/master/bip-0320.mediawiki) bits to any
+    /// particular value.
     pub version: u32,
-    /// The coinbase transaction nVersion field.
+    /// The coinbase transaction `nVersion` field.
     pub coinbase_tx_version: u32,
-    /// Up to 8 bytes (not including the length byte) which are to be placed
-    /// at the beginning of the coinbase field in the coinbase transaction.
+    /// Up to 8 bytes (not including the length byte) which are to be placed at the beginning of
+    /// the coinbase field in the coinbase transaction.
     #[cfg_attr(feature = "with_serde", serde(borrow))]
     pub coinbase_prefix: B0255<'decoder>,
-    ///bug
-    /// The coinbase transaction input’s nSequence field.
+    /// The coinbase transaction input’s `nSequence` field.
     pub coinbase_tx_input_sequence: u32,
-    /// The value, in satoshis, available for spending in coinbase outputs
-    /// added by the client. Includes both transaction fees and block
-    /// subsidy.
+    /// The value, in satoshis, available for spending in coinbase outputs added by the downstream.
+    ///
+    /// Includes both transaction fees and block subsidy.
     pub coinbase_tx_value_remaining: u64,
-    /// The number of transaction outputs included in coinbase_tx_outputs.
+    /// The number of transaction outputs included in [`NewTemplate::coinbase_tx_outputs`].
     pub coinbase_tx_outputs_count: u32,
-    /// Bitcoin transaction outputs to be included as the last outputs in the
-    /// coinbase transaction.
+    /// Bitcoin transaction outputs to be included as the last outputs in the coinbase transaction.
+    ///
+    /// Note that those bytes will appear as is at the end of the coinbase transaction.
     #[cfg_attr(feature = "with_serde", serde(borrow))]
     pub coinbase_tx_outputs: B064K<'decoder>,
-    /// The locktime field in the coinbase transaction.
+    /// The `locktime` field in the coinbase transaction.
     pub coinbase_tx_locktime: u32,
     /// Merkle path hashes ordered from deepest.
     #[cfg_attr(feature = "with_serde", serde(borrow))]
     pub merkle_path: Seq0255<'decoder, U256<'decoder>>,
 }
 
+/// C representation of [`NewTemplate`].
 #[repr(C)]
 #[cfg(not(feature = "with_serde"))]
 pub struct CNewTemplate {
@@ -71,6 +77,7 @@ pub struct CNewTemplate {
     merkle_path: CVec2,
 }
 
+/// Drops the [`CNewTemplate`] object.
 #[no_mangle]
 #[cfg(not(feature = "with_serde"))]
 pub extern "C" fn free_new_template(s: CNewTemplate) {
@@ -107,6 +114,7 @@ impl<'a> From<NewTemplate<'a>> for CNewTemplate {
 
 #[cfg(not(feature = "with_serde"))]
 impl<'a> CNewTemplate {
+    /// Converts from C to Rust representation.
     #[cfg(not(feature = "with_serde"))]
     #[allow(clippy::wrong_self_convention)]
     pub fn to_rust_rep_mut(&'a mut self) -> Result<NewTemplate<'a>, Error> {
