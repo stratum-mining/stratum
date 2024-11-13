cdcc9c8a86475ec1c9fda9d4102884f90cf09323
fi3 <email@email.com>
2024-11-13 12:48:52 +0100

    Update channel factory, coinbase input script handling.
    Coinbase signature is not part of the Sv2 protocol, some pool maybe want
    to use it other not. The first part of the extranonce could also be
    reserved for things that are not a pool signature. This pr rename the
    pool_signature field of the channel factory into additional_coinbase_script_data
    and change the type from Strgin to Vec<u8>, since can be anything.

--- protocols/v2/roles-logic-sv2/src/channel_logic/channel_factory.rs
+++ protocols/v2/roles-logic-sv2/src/channel_logic/channel_factory.rs
@@ -975,7 +975,9 @@ pub struct PoolChannelFactory {
     inner: ChannelFactory,
     job_creator: JobsCreators,
     pool_coinbase_outputs: Vec<TxOut>,
-    pool_signature: String,
+    // Additional data that the pool may want to include in the coinbase input script as first part
+    // of the extranonce. This can be used to put things like the pool signature.
+    additional_coinbase_script_data: Vec<u8>,
     // extedned_channel_id -> SetCustomMiningJob
     negotiated_jobs: HashMap<u32, SetCustomMiningJob<'static>, BuildNoHashHasher<u32>>,
 }
@@ -988,7 +990,7 @@ impl PoolChannelFactory {
         share_per_min: f32,
         kind: ExtendedChannelKind,
         pool_coinbase_outputs: Vec<TxOut>,
-        pool_signature: String,
+        additional_coinbase_script_data: Vec<u8>,
     ) -> Self {
         let inner = ChannelFactory {
             ids,
@@ -1015,7 +1017,7 @@ impl PoolChannelFactory {
             inner,
             job_creator,
             pool_coinbase_outputs,
-            pool_signature,
+            additional_coinbase_script_data,
             negotiated_jobs: HashMap::with_hasher(BuildNoHashHasher::default()),
         }
     }
@@ -1083,7 +1085,7 @@ impl PoolChannelFactory {
             m,
             true,
             self.pool_coinbase_outputs.clone(),
-            self.pool_signature.clone(),
+            &self.additional_coinbase_script_data,
         )?;
         self.inner.on_new_extended_mining_job(new_job)
     }
@@ -1160,10 +1162,13 @@ impl PoolChannelFactory {
         if self.negotiated_jobs.contains_key(&m.channel_id) {
             let referenced_job = self.negotiated_jobs.get(&m.channel_id).unwrap();
             let merkle_path = referenced_job.merkle_path.to_vec();
-            let pool_signature = self.pool_signature.clone();
-            let extended_job =
-                job_creator::extended_job_from_custom_job(referenced_job, pool_signature, 32)
-                    .unwrap();
+            let additional_coinbase_script_data = self.additional_coinbase_script_data.clone();
+            let extended_job = job_creator::extended_job_from_custom_job(
+                referenced_job,
+                additional_coinbase_script_data.as_ref(),
+                32,
+            )
+            .unwrap();
             let prev_blockhash = crate::utils::u256_to_block_hash(referenced_job.prev_hash.clone());
             let bits = referenced_job.nbits;
             self.inner.check_target(
@@ -1295,7 +1300,7 @@ pub struct ProxyExtendedChannelFactory {
     inner: ChannelFactory,
     job_creator: Option<JobsCreators>,
     pool_coinbase_outputs: Option<Vec<TxOut>>,
-    pool_signature: String,
+    additional_coinbase_script_data: String,
     // Id assigned to the extended channel by upstream
     extended_channel_id: u32,
 }
@@ -1309,7 +1314,7 @@ impl ProxyExtendedChannelFactory {
         share_per_min: f32,
         kind: ExtendedChannelKind,
         pool_coinbase_outputs: Option<Vec<TxOut>>,
-        pool_signature: String,
+        additional_coinbase_script_data: String,
         extended_channel_id: u32,
     ) -> Self {
         match &kind {
@@ -1349,7 +1354,7 @@ impl ProxyExtendedChannelFactory {
             inner,
             job_creator,
             pool_coinbase_outputs,
-            pool_signature,
+            additional_coinbase_script_data,
             extended_channel_id,
         }
     }
@@ -1444,7 +1449,7 @@ impl ProxyExtendedChannelFactory {
                 m,
                 true,
                 pool_coinbase_outputs.clone(),
-                self.pool_signature.clone(),
+                self.additional_coinbase_script_data.as_ref(),
             )?;
             let id = new_job.job_id;
             if !new_job.is_future() && self.inner.last_prev_hash.is_some() {
@@ -1862,7 +1867,7 @@ mod test {
 
         // Initialize a Channel of type Pool
         let out = TxOut {value: BLOCK_REWARD, script_pubkey: decode_hex("4104c6d0969c2d98a5c19ba7c36c7937c5edbd60ff2a01397c4afe54f16cd641667ea0049ba6f9e1796ba3c8e49e1b504c532ebbaaa1010c3f7d9b83a8ea7fd800e2ac").unwrap().into()};
-        let pool_signature = "".to_string();
+        let additional_coinbase_script_data = "".to_string();
         let creator = JobsCreators::new(7);
         let share_per_min = 1.0;
         // Create an ExtendedExtranonce of len 7:
@@ -1883,7 +1888,7 @@ mod test {
             share_per_min,
             channel_kind,
             vec![out],
-            pool_signature,
+            additional_coinbase_script_data,
         );
 
         // Build a NewTemplate
--- protocols/v2/roles-logic-sv2/src/job_creator.rs
+++ protocols/v2/roles-logic-sv2/src/job_creator.rs
@@ -70,7 +70,7 @@ impl JobsCreators {
         template: &mut NewTemplate,
         version_rolling_allowed: bool,
         mut pool_coinbase_outputs: Vec<TxOut>,
-        pool_signature: String,
+        additional_coinbase_script_data: &[u8],
     ) -> Result<NewExtendedMiningJob<'static>, Error> {
         let server_tx_outputs = template.coinbase_tx_outputs.to_vec();
         let mut outputs = tx_outputs_to_costum_scripts(&server_tx_outputs);
@@ -87,7 +87,7 @@ impl JobsCreators {
         new_extended_job(
             template,
             &mut pool_coinbase_outputs,
-            pool_signature,
+            additional_coinbase_script_data,
             next_job_id,
             version_rolling_allowed,
             self.extranonce_len,
@@ -137,7 +137,7 @@ impl JobsCreators {
 
 pub fn extended_job_from_custom_job(
     referenced_job: &mining_sv2::SetCustomMiningJob,
-    pool_signature: String,
+    additional_coinbase_script_data: &[u8],
     extranonce_len: u8,
 ) -> Result<NewExtendedMiningJob<'static>, Error> {
     let mut outputs =
@@ -158,7 +158,7 @@ pub fn extended_job_from_custom_job(
     new_extended_job(
         &mut template,
         &mut outputs,
-        pool_signature,
+        additional_coinbase_script_data,
         0,
         true,
         extranonce_len,
@@ -177,7 +177,7 @@ pub fn extended_job_from_custom_job(
 fn new_extended_job(
     new_template: &mut NewTemplate,
     coinbase_outputs: &mut [TxOut],
-    pool_signature: String,
+    additional_coinbase_script_data: &[u8],
     job_id: u32,
     version_rolling_allowed: bool,
     extranonce_len: u8,
@@ -193,7 +193,7 @@ fn new_extended_job(
         .map_err(|_| Error::TxVersionTooBig)?;
 
     let bip34_bytes = get_bip_34_bytes(new_template, tx_version)?;
-    let script_prefix_len = bip34_bytes.len() + pool_signature.as_bytes().len();
+    let script_prefix_len = bip34_bytes.len() + additional_coinbase_script_data.len();
 
     let coinbase = coinbase(
         bip34_bytes,
@@ -201,7 +201,7 @@ fn new_extended_job(
         new_template.coinbase_tx_locktime,
         new_template.coinbase_tx_input_sequence,
         coinbase_outputs,
-        pool_signature,
+        additional_coinbase_script_data,
         extranonce_len,
     );
 
@@ -327,7 +327,7 @@ fn coinbase(
     lock_time: u32,
     sequence: u32,
     coinbase_outputs: &[TxOut],
-    pool_signature: String,
+    additional_coinbase_script_data: &[u8],
     extranonce_len: u8,
 ) -> Transaction {
     // If script_prefix_len is not 0 we are not in a test enviornment and the coinbase have the 0
@@ -336,7 +336,7 @@ fn coinbase(
         0 => Witness::from_vec(vec![]),
         _ => Witness::from_vec(vec![vec![0; 32]]),
     };
-    bip34_bytes.extend_from_slice(pool_signature.as_bytes());
+    bip34_bytes.extend_from_slice(additional_coinbase_script_data);
     bip34_bytes.extend_from_slice(&vec![0; extranonce_len as usize]);
     let tx_in = TxIn {
         previous_output: OutPoint::null(),
