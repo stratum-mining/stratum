627506e3615bcc82ff0edad883867bb6ad636f8b
GitGab19 <gabriele.vernetti@protonmail.com>
2024-11-15 16:56:04 +0100

    handlers docs

--- protocols/v2/roles-logic-sv2/src/handlers/mining.rs
+++ protocols/v2/roles-logic-sv2/src/handlers/mining.rs
@@ -1,3 +1,41 @@
+//! # Mining Handlers
+//!
+//! This module defines traits and functions for handling mining-related messages within the Stratum
+//! V2 protocol.
+//!
+//! ## Core Traits
+//!
+//! - `ParseUpstreamMiningMessages`: Implemented by downstream nodes to process mining messages
+//!   received from upstream nodes. This trait provides methods for handling mining events like new
+//!   mining jobs, share submissions, extranonce prefix updates, and channel status updates.
+//! - `ParseDownstreamMiningMessages`: Implemented by upstream nodes to manage mining messages
+//!   received from downstream nodes. This trait includes methods for managing tasks such as
+//!   submitting shares, opening mining channels, and handling mining job responses.
+//!
+//! ## Message Handling
+//!
+//! Handlers in this module are responsible for:
+//! - Parsing and deserializing mining-related messages into the appropriate types.
+//! - Dispatching the deserialized messages to specific handler functions based on message type,
+//!   such as handling new mining jobs, share submissions, and extranonce updates.
+//! - Ensuring the integrity and validity of received messages, while interacting with downstream
+//!   mining systems to ensure proper communication and task execution.
+//!
+//! ## Return Type
+//!
+//! Functions return `Result<SendTo<Down>, Error>`, where `SendTo<Down>` specifies the next action
+//! for the message: whether it should be sent to the downstream node, an error response should be
+//! generated, or the message should be ignored.
+//!
+//! ## Structure
+//!
+//! This module includes:
+//! - Traits for processing mining-related messages for both upstream and downstream communication.
+//! - Functions to parse, deserialize, and process messages related to mining, ensuring robust error
+//!   handling for unexpected conditions.
+//! - Support for managing mining channels, extranonce prefixes, and share submissions, while
+//!   handling edge cases and ensuring the correctness of the mining process.
+
 use crate::{common_properties::RequestIdMapper, errors::Error, parsers::Mining};
 use core::convert::TryInto;
 use mining_sv2::{
@@ -24,16 +62,20 @@ use tracing::{debug, error, info, trace};
 
 pub type SendTo<Remote> = SendTo_<Mining<'static>, Remote>;
 
+/// Represents supported channel types in a mining connection.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum SupportedChannelTypes {
     Standard,
     Extended,
     Group,
-    // Non header only connection can support both group and extended channels.
+    /// Represents a connection that supports both group and extended channels.
     GroupAndExtended,
 }
 
-/// Connection-wide downtream's messages parser implemented by an upstream.
+/// Trait for parsing downstream mining messages in a Stratum V2 connection.
+///
+/// This trait defines methods for parsing and routing downstream messages
+/// related to mining operations.
 pub trait ParseDownstreamMiningMessages<
     Up: IsMiningUpstream<Self, Selector> + D,
     Selector: DownstreamMiningSelector<Self> + D,
@@ -41,10 +83,19 @@ pub trait ParseDownstreamMiningMessages<
 > where
     Self: IsMiningDownstream + Sized + D,
 {
+    /// Returns the type of channel supported by the downstream connection.
     fn get_channel_type(&self) -> SupportedChannelTypes;
 
-    /// Used to parse and route SV2 mining messages from the downstream based on `message_type` and
-    /// `payload`
+    /// Handles a mining message from the downstream, given its type and payload.
+    ///
+    /// # Arguments
+    /// - `self_mutex`: The `Arc<Mutex<Self>>` representing the downstream entity.
+    /// - `message_type`: The type of the mining message.
+    /// - `payload`: The raw payload of the message.
+    /// - `routing_logic`: The logic for routing the message to the appropriate upstream entity.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Up>, Error>`: The result of processing the message.
     fn handle_message_mining(
         self_mutex: Arc<Mutex<Self>>,
         message_type: u8,
@@ -64,7 +115,15 @@ pub trait ParseDownstreamMiningMessages<
         }
     }
 
-    /// Used to route SV2 mining messages from the downstream
+    /// Deserializes and processes a mining message from the downstream.
+    ///
+    /// # Arguments
+    /// - `self_mutex`: The `Arc<Mutex<Self>>` representing the downstream entity.
+    /// - `message`: The mining message to be processed.
+    /// - `routing_logic`: The logic for routing the message to the appropriate upstream entity.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Up>, Error>`: The result of processing the message.
     fn handle_message_mining_deserialized(
         self_mutex: Arc<Mutex<Self>>,
         message: Result<Mining<'_>, Error>,
@@ -265,9 +324,20 @@ pub trait ParseDownstreamMiningMessages<
         }
     }
 
+    /// Checks if work selection is enabled for the downstream connection.
+    ///
+    /// # Returns
+    /// - `bool`: `true` if work selection is enabled, `false` otherwise.
     fn is_work_selection_enabled(&self) -> bool;
 
-    /// returns None if the user is authorized and Open
+    /// Checks if the downstream user is authorized.
+    ///
+    /// # Arguments
+    /// - `_self_mutex`: The `Arc<Mutex<Self>>` representing the downstream entity.
+    /// - `_user_identity`: The user's identity to be checked.
+    ///
+    /// # Returns
+    /// - `Result<bool, Error>`: `true` if the user is authorized, `false` otherwise.
     fn is_downstream_authorized(
         _self_mutex: Arc<Mutex<Self>>,
         _user_identity: &binary_sv2::Str0255,
@@ -275,32 +345,94 @@ pub trait ParseDownstreamMiningMessages<
         Ok(true)
     }
 
+    /// Handles an `OpenStandardMiningChannel` message.
+    ///
+    /// This method processes an `OpenStandardMiningChannel` message and initiates the
+    /// appropriate response.
+    ///
+    /// # Arguments
+    /// - `m`: The `OpenStandardMiningChannel` message.
+    /// - `up`: An optional upstream entity to which the message is forwarded.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Up>, Error>`: The result of processing the message.
     fn handle_open_standard_mining_channel(
         &mut self,
         m: OpenStandardMiningChannel,
         up: Option<Arc<Mutex<Up>>>,
     ) -> Result<SendTo<Up>, Error>;
 
+    /// Handles an `OpenExtendedMiningChannel` message.
+    ///
+    /// This method processes an `OpenExtendedMiningChannel` message and initiates the
+    /// appropriate response.
+    ///
+    /// # Arguments
+    /// - `m`: The `OpenExtendedMiningChannel` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Up>, Error>`: The result of processing the message.
     fn handle_open_extended_mining_channel(
         &mut self,
         m: OpenExtendedMiningChannel,
     ) -> Result<SendTo<Up>, Error>;
 
+    /// Handles an `UpdateChannel` message.
+    ///
+    /// This method processes an `UpdateChannel` message and updates the channel settings.
+    ///
+    /// # Arguments
+    /// - `m`: The `UpdateChannel` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Up>, Error>`: The result of processing the message.
     fn handle_update_channel(&mut self, m: UpdateChannel) -> Result<SendTo<Up>, Error>;
 
+    /// Handles a `SubmitSharesStandard` message.
+    ///
+    /// This method processes a `SubmitSharesStandard` message and validates the submitted shares.
+    ///
+    /// # Arguments
+    /// - `m`: The `SubmitSharesStandard` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Up>, Error>`: The result of processing the message.
     fn handle_submit_shares_standard(
         &mut self,
         m: SubmitSharesStandard,
     ) -> Result<SendTo<Up>, Error>;
 
+    /// Handles a `SubmitSharesExtended` message.
+    ///
+    /// This method processes a `SubmitSharesExtended` message and validates the submitted shares.
+    ///
+    /// # Arguments
+    /// - `m`: The `SubmitSharesExtended` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Up>, Error>`: The result of processing the message.
     fn handle_submit_shares_extended(
         &mut self,
         m: SubmitSharesExtended,
     ) -> Result<SendTo<Up>, Error>;
 
+    /// Handles a `SetCustomMiningJob` message.
+    ///
+    /// This method processes a `SetCustomMiningJob` message and applies the custom mining job
+    /// settings.
+    ///
+    /// # Arguments
+    /// - `m`: The `SetCustomMiningJob` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Up>, Error>`: The result of processing the message.
     fn handle_set_custom_mining_job(&mut self, m: SetCustomMiningJob) -> Result<SendTo<Up>, Error>;
 }
-/// Connection-wide upstream's messages parser implemented by a downstream.
+
+/// A trait defining the parser for upstream mining messages used by a downstream.
+///
+/// This trait provides the functionality to handle and route various types of mining messages
+/// from the upstream based on the message type and payload.
 pub trait ParseUpstreamMiningMessages<
     Down: IsMiningDownstream + D,
     Selector: DownstreamMiningSelector<Down> + D,
@@ -308,16 +440,35 @@ pub trait ParseUpstreamMiningMessages<
 > where
     Self: IsMiningUpstream<Down, Selector> + Sized + D,
 {
+    /// Retrieves the type of the channel supported by this upstream parser.
+    ///
+    /// # Returns
+    /// - `SupportedChannelTypes`: The supported channel type for this upstream.
     fn get_channel_type(&self) -> SupportedChannelTypes;
 
+    /// Retrieves an optional RequestIdMapper, used to manage request IDs across connections.
+    ///
+    /// # Returns
+    /// - `Option<Arc<Mutex<RequestIdMapper>>>`: An optional RequestIdMapper for request ID
+    ///   modification.
     fn get_request_id_mapper(&mut self) -> Option<Arc<Mutex<RequestIdMapper>>> {
         None
     }
 
-    /// Used to parse and route SV2 mining messages from the upstream based on `message_type` and
-    /// `payload` The implementor of DownstreamMining needs to pass a RequestIdMapper if needing
-    /// to change the req id. Proxies likely would want to update a downstream req id to a new
-    /// one as req id must be connection-wide unique
+    /// Parses and routes SV2 mining messages from the upstream based on the message type and
+    /// payload. The implementor of DownstreamMining needs to pass a RequestIdMapper if changing
+    /// the request ID. Proxies typically need this to ensure the request ID is unique across
+    /// the connection.
+    ///
+    /// # Arguments
+    /// - `self_mutex`: The `Arc<Mutex<Self>>` representing the downstream entity.
+    /// - `message_type`: The type of the incoming message.
+    /// - `payload`: The payload containing the message data.
+    /// - `routing_logic`: The logic to handle the routing of the message based on the type.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the message, either sending a
+    ///   response or an error.
     fn handle_message_mining(
         self_mutex: Arc<Mutex<Self>>,
         message_type: u8,
@@ -334,6 +485,17 @@ pub trait ParseUpstreamMiningMessages<
         }
     }
 
+    /// Handles the deserialized mining message from the upstream, processing it according to the
+    /// routing logic.
+    ///
+    /// # Arguments
+    /// - `self_mutex`: The `Arc<Mutex<Self>>` representing the downstream entity.
+    /// - `message`: The deserialized mining message, wrapped in a Result for error handling.
+    /// - `routing_logic`: The logic used to route the message based on the type.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the message, either sending a
+    ///   response or an error.
     fn handle_message_mining_deserialized(
         self_mutex: Arc<Mutex<Self>>,
         message: Result<Mining, Error>,
@@ -673,64 +835,181 @@ pub trait ParseUpstreamMiningMessages<
         }
     }
 
+    /// Determines whether work selection is enabled for this upstream.
+    ///
+    /// # Returns
+    /// - `bool`: A boolean indicating if work selection is enabled.
     fn is_work_selection_enabled(&self) -> bool;
 
+    /// Handles a successful response for opening a standard mining channel.
+    ///
+    /// # Arguments
+    /// - `m`: The `OpenStandardMiningChannelSuccess` message.
+    /// - `remote`: An optional reference to the downstream, wrapped in an `Arc<Mutex>`.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the message.
     fn handle_open_standard_mining_channel_success(
         &mut self,
         m: OpenStandardMiningChannelSuccess,
         remote: Option<Arc<Mutex<Down>>>,
     ) -> Result<SendTo<Down>, Error>;
 
+    /// Handles a successful response for opening an extended mining channel.
+    ///
+    /// # Arguments
+    /// - `m`: The `OpenExtendedMiningChannelSuccess` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the message.
     fn handle_open_extended_mining_channel_success(
         &mut self,
         m: OpenExtendedMiningChannelSuccess,
     ) -> Result<SendTo<Down>, Error>;
 
+    /// Handles an error when opening a mining channel.
+    ///
+    /// # Arguments
+    /// - `m`: The `OpenMiningChannelError` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the error.
     fn handle_open_mining_channel_error(
         &mut self,
         m: OpenMiningChannelError,
     ) -> Result<SendTo<Down>, Error>;
 
+    /// Handles an error when updating a mining channel.
+    ///
+    /// # Arguments
+    /// - `m`: The `UpdateChannelError` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the error.
     fn handle_update_channel_error(&mut self, m: UpdateChannelError)
         -> Result<SendTo<Down>, Error>;
 
+    /// Handles a request to close a mining channel.
+    ///
+    /// # Arguments
+    /// - `m`: The `CloseChannel` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the message.
     fn handle_close_channel(&mut self, m: CloseChannel) -> Result<SendTo<Down>, Error>;
 
+    /// Handles a request to set the extranonce prefix for mining.
+    ///
+    /// # Arguments
+    /// - `m`: The `SetExtranoncePrefix` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the message.
     fn handle_set_extranonce_prefix(
         &mut self,
         m: SetExtranoncePrefix,
     ) -> Result<SendTo<Down>, Error>;
 
+    /// Handles a successful submission of shares.
+    ///
+    /// # Arguments
+    /// - `m`: The `SubmitSharesSuccess` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the message.
     fn handle_submit_shares_success(
         &mut self,
         m: SubmitSharesSuccess,
     ) -> Result<SendTo<Down>, Error>;
 
+    /// Handles an error when submitting shares.
+    ///
+    /// # Arguments
+    /// - `m`: The `SubmitSharesError` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the error.
     fn handle_submit_shares_error(&mut self, m: SubmitSharesError) -> Result<SendTo<Down>, Error>;
 
+    /// Handles a new mining job.
+    ///
+    /// # Arguments
+    /// - `m`: The `NewMiningJob` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the message.
     fn handle_new_mining_job(&mut self, m: NewMiningJob) -> Result<SendTo<Down>, Error>;
 
+    /// Handles a new extended mining job.
+    ///
+    /// # Arguments
+    /// - `m`: The `NewExtendedMiningJob` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the message.
     fn handle_new_extended_mining_job(
         &mut self,
         m: NewExtendedMiningJob,
     ) -> Result<SendTo<Down>, Error>;
 
+    /// Handles a request to set the new previous hash.
+    ///
+    /// # Arguments
+    /// - `m`: The `SetNewPrevHash` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the message.
     fn handle_set_new_prev_hash(&mut self, m: SetNewPrevHash) -> Result<SendTo<Down>, Error>;
 
+    /// Handles a successful response for setting a custom mining job.
+    ///
+    /// # Arguments
+    /// - `m`: The `SetCustomMiningJobSuccess` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the message.
     fn handle_set_custom_mining_job_success(
         &mut self,
         m: SetCustomMiningJobSuccess,
     ) -> Result<SendTo<Down>, Error>;
 
+    /// Handles an error when setting a custom mining job.
+    ///
+    /// # Arguments
+    /// - `m`: The `SetCustomMiningJobError` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the error.
     fn handle_set_custom_mining_job_error(
         &mut self,
         m: SetCustomMiningJobError,
     ) -> Result<SendTo<Down>, Error>;
 
+    /// Handles a request to set the target for mining.
+    ///
+    /// # Arguments
+    /// - `m`: The `SetTarget` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the message.
     fn handle_set_target(&mut self, m: SetTarget) -> Result<SendTo<Down>, Error>;
 
+    /// Handles a request to reconnect the mining connection.
+    ///
+    /// # Arguments
+    /// - `m`: The `Reconnect` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the message.
     fn handle_reconnect(&mut self, m: Reconnect) -> Result<SendTo<Down>, Error>;
 
+    /// Handles a request to set the group channel for mining.
+    ///
+    /// # Arguments
+    /// - `_m`: The `SetGroupChannel` message.
+    ///
+    /// # Returns
+    /// - `Result<SendTo<Down>, Error>`: The result of processing the message.
     fn handle_set_group_channel(&mut self, _m: SetGroupChannel) -> Result<SendTo<Down>, Error> {
         Ok(SendTo::None(None))
     }
