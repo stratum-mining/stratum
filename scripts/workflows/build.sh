06a78b7df5eda1fbbcbd6a5dcb6d40d30aaf62b3
devworlds <jeffluizblockchain@gmail.com>
2025-01-06 14:40:08 -0300

    chore: add loop to build.sh, keep only tests with loop on test.sh, and create a new script with examples with new job ci-example.

--- scripts/workflows/build.sh
+++ scripts/workflows/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cargo build --manifest-path=benches/Cargo.toml
-cargo build --manifest-path=protocols/Cargo.toml
-cargo build --manifest-path=roles/Cargo.toml
-cargo build --manifest-path=utils/Cargo.toml
+for manifest in \
+  benches/Cargo.toml \
+  protocols/Cargo.toml \
+  roles/Cargo.toml \
+  utils/Cargo.toml; do
+    cargo build --manifest-path="$manifest"
+done
--- scripts/workflows/test.sh
+++ scripts/workflows/test.sh
@@ -1,34 +1,12 @@
 #!/bin/bash
-cargo test --manifest-path=roles/Cargo.toml --verbose --test '*' -- --nocapture
 
-cargo run --manifest-path=examples/sv1-client-and-server/Cargo.toml --bin client_and_server -- 60
-
-if [ "$CI_OS" == "ubuntu-latest" ]; then
-    ./run.sh 30
-else
-    echo "Skipping interop-test on $CI_OS - not supported"
-fi
-
-cd examples/interop-cpp/ || exit
-
-if [ "$CI_OS" == "ubuntu-latest" ]; then
-    ./run.sh 30
-else
-    echo "Skipping fuzz test on $CI_OS - not supported"
-fi
-
-cd ../..
-cd utils/buffer/fuzz/ || exit
-
-cd ../../..
-cargo test --manifest-path=benches/Cargo.toml
-cargo test --manifest-path=common/Cargo.toml
-cargo test --manifest-path=protocols/Cargo.toml
-cargo test --manifest-path=roles/Cargo.toml
-cargo test --manifest-path=utils/Cargo.toml
+for manifest in \
+  benches/Cargo.toml \
+  common/Cargo.toml \
+  protocols/Cargo.toml \
+  roles/Cargo.toml \
+  utils/Cargo.toml; do
+    cargo test --manifest-path="$manifest"
+done
 
 cargo test --manifest-path=protocols/Cargo.toml --features prop_test
-
-cargo run --manifest-path=examples/ping-pong-with-noise/Cargo.toml --bin ping_pong_with_noise -- 10
-
-cargo run --manifest-path=examples/ping-pong-without-noise/Cargo.toml --bin ping_pong_without_noise -- 10
