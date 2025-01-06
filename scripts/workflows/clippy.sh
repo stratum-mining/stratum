8b3ab7f3914cf988a99c64ecfc4147e3de4635c2
devworlds <jeffluizblockchain@gmail.com>
2025-01-06 09:25:41 -0300

    chore: adjust wrong clippy.sh file.

--- scripts/workflows/clippy.sh
+++ scripts/workflows/clippy.sh
@@ -1,28 +1,10 @@
-#!/bin/sh
-
-WORKSPACES="benches common protocols roles utils"
-
-for workspace in $WORKSPACES; do
-    echo "Executing clippy on: $workspace"
-    cargo +1.75.0 clippy --manifest-path="$workspace/Cargo.toml" -- -D warnings -A dead-code
-    if [ $? -ne 0 ]; then
-        echo "Clippy found some errors in: $workspace"
-        exit 1
-    fi
-
-    echo "Running tests on: $workspace"
-    cargo +1.75 test --manifest-path="$workspace/Cargo.toml"
-    if [ $? -ne 0 ]; then
-        echo "Tests failed in: $workspace"
-        exit 1
-    fi
-
-    echo "Running fmt on: $workspace"
-    (cd $workspace && cargo +nightly fmt)
-    if [ $? -ne 0 ]; then
-        echo "Fmt failed in: $workspace"
-        exit 1
-    fi
+#!/bin/bash
+for manifest in \
+  benches/Cargo.toml \
+  common/Cargo.toml \
+  protocols/Cargo.toml \
+  roles/Cargo.toml \
+  utils/Cargo.toml \
+  utils/message-generator/Cargo.toml; do
+    cargo clippy --manifest-path=$manifest -- -D warnings -A dead-code
 done
-
-echo "Clippy success, all tests passed!"
