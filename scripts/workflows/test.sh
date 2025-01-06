3179e7bd1332928f3330aed99629a37f95009d0d
devworlds <jeffluizblockchain@gmail.com>
2025-01-06 11:31:44 -0300

    chore: remove comments, and add perm to test.sh

--- scripts/workflows/test.sh
+++ scripts/workflows/test.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# Instala toolchain e componentes
 cargo build --manifest-path=benches/Cargo.toml
 cargo build --manifest-path=common/Cargo.toml
 cargo build --manifest-path=protocols/Cargo.toml
 cargo build --manifest-path=roles/Cargo.toml
 cargo build --manifest-path=utils/Cargo.toml
 
-# Testes de Integração de Roles
 cargo test --manifest-path=roles/Cargo.toml --verbose --test '*' -- --nocapture
 
-# Executa o exemplo de client_and_server
 cargo run --manifest-path=examples/sv1-client-and-server/Cargo.toml --bin client_and_server -- 60
 
-# Teste de Interop (somente no Ubuntu)
 if [ "$CI_OS" == "ubuntu-latest" ]; then
     ./run.sh 30
 else
@@ -21,7 +17,6 @@ else
 fi
 cd examples/interop-cpp/ || exit
 
-# Teste de fuzz (somente no Ubuntu)
 if [ "$CI_OS" == "ubuntu-latest" ]; then
     ./run.sh 30
 else
@@ -29,18 +24,14 @@ else
 fi
 cd utils/buffer/fuzz || exit
 
-# Testes gerais
 cargo test --manifest-path=benches/Cargo.toml
 cargo test --manifest-path=common/Cargo.toml
 cargo test --manifest-path=protocols/Cargo.toml
 cargo test --manifest-path=roles/Cargo.toml
 cargo test --manifest-path=utils/Cargo.toml
 
-# Testes baseados em propriedades
 cargo test --manifest-path=protocols/Cargo.toml --features prop_test
 
-# Executa o exemplo de ping-pong-with-noise
 cargo run --manifest-path=examples/ping-pong-with-noise/Cargo.toml --bin ping_pong_with_noise -- 10
 
-# Executa o exemplo de ping-pong-without-noise
 cargo run --manifest-path=examples/ping-pong-without-noise/Cargo.toml --bin ping_pong_without_noise -- 10
