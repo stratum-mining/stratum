43e204be850c9ba5389af446a08d214911c18065
jbesraa <jbesraa@gmail.com>
2025-01-09 17:41:09 +0200

    Add `Sniffer::wait_for_message_type_and_clean_queue`..
    fn

--- roles/tests-integration/tests/sniffer_integration.rs
+++ roles/tests-integration/tests/sniffer_integration.rs
@@ -1,4 +1,7 @@
-use const_sv2::MESSAGE_TYPE_SETUP_CONNECTION_ERROR;
+use const_sv2::{
+    MESSAGE_TYPE_SETUP_CONNECTION_ERROR, MESSAGE_TYPE_SETUP_CONNECTION_SUCCESS,
+    MESSAGE_TYPE_SET_NEW_PREV_HASH,
+};
 use integration_tests_sv2::*;
 use roles_logic_sv2::{
     common_messages_sv2::SetupConnectionError,
@@ -10,7 +13,6 @@ use std::convert::TryInto;
 #[tokio::test]
 async fn test_sniffer_interrupter() {
     let (_tp, tp_addr) = start_template_provider(None).await;
-    use const_sv2::MESSAGE_TYPE_SETUP_CONNECTION_SUCCESS;
     let message =
         PoolMessages::Common(CommonMessages::SetupConnectionError(SetupConnectionError {
             flags: 0,
@@ -32,4 +34,37 @@ async fn test_sniffer_interrupter() {
     let _ = start_pool(Some(sniffer_addr)).await;
     assert_common_message!(&sniffer.next_message_from_downstream(), SetupConnection);
     assert_common_message!(&sniffer.next_message_from_upstream(), SetupConnectionError);
+}
+
+#[tokio::test]
+async fn test_sniffer_wait_for_message_type_with_remove() {
+    let (_tp, tp_addr) = start_template_provider(None).await;
+    let (sniffer, sniffer_addr) = start_sniffer("".to_string(), tp_addr, false, None).await;
+    let _ = start_pool(Some(sniffer_addr)).await;
+    assert!(
+        sniffer
+            .wait_for_message_type_and_clean_queue(
+                MessageDirection::ToDownstream,
+                MESSAGE_TYPE_SET_NEW_PREV_HASH,
+            )
+            .await
+    );
+    assert_eq!(
+        sniffer
+            .includes_message_type(
+                MessageDirection::ToDownstream,
+                MESSAGE_TYPE_SETUP_CONNECTION_SUCCESS
+            )
+            .await,
+        false
+    );
+    assert_eq!(
+        sniffer
+            .includes_message_type(
+                MessageDirection::ToDownstream,
+                MESSAGE_TYPE_SET_NEW_PREV_HASH
+            )
+            .await,
+        false
+    );
 }
