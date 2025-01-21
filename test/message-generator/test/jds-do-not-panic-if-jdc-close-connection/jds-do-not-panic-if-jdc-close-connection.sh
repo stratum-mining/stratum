263183e82f8238ba1a38c1693df111bdad3cc79a
bit-aloo <sshourya17@gmail.com>
2025-01-21 20:41:34 +0530

    remove jd-does-not-panic-if-jdc-disconnect from ci's mg yaml and  mg test from test module

--- test/message-generator/test/jds-do-not-panic-if-jdc-close-connection/jds-do-not-panic-if-jdc-close-connection.sh
+++
@@ -1,9 +0,0 @@
-cd roles
-cargo llvm-cov --no-report -p jd_server
-
-cd ../utils/message-generator/
-cargo build
-
-RUST_LOG=debug cargo run ../../test/message-generator/test/jds-do-not-panic-if-jdc-close-connection/jds-do-not-panic-if-jdc-close-connection.json || { echo 'mg test failed' ; exit 1; }
-
-sleep 10
