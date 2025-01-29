353d97d73dfe6552b011a4e9c4ebe721c424a579
plebhash <plebhash@proton.me>
2025-01-29 13:28:37 -0300

    add tproxy_refuses_bad_extranonce_size integration test

--- test/integration-tests/tests/translator_integration.rs
+++ test/integration-tests/tests/translator_integration.rs
@@ -6,10 +6,14 @@
 // all tests. This is because tracing is a global setting.
 use const_sv2::{
     MESSAGE_TYPE_SETUP_CONNECTION, MESSAGE_TYPE_SUBMIT_SHARES_EXTENDED,
-    MESSAGE_TYPE_SUBMIT_SHARES_SUCCESS,
+    MESSAGE_TYPE_SUBMIT_SHARES_SUCCESS, MESSAGE_TYPE_OPEN_EXTENDED_MINING_CHANNEL_SUCCES
 };
 use integration_tests_sv2::{sniffer::*, *};
-use roles_logic_sv2::parsers::{AnyMessage, CommonMessages, Mining};
+use roles_logic_sv2::parsers::{
+    AnyMessage, CommonMessages, Mining,
+};
+use roles_logic_sv2::mining_sv2::OpenExtendedMiningChannelSuccess;
+use std::convert::TryInto;
 
 // This test runs an sv2 translator between an sv1 mining device and a pool. the connection between
 // the translator and the pool is intercepted by a sniffer. The test checks if the translator and
@@ -103,4 +107,43 @@ async fn translation_proxy_and_jd() {
             MESSAGE_TYPE_SUBMIT_SHARES_SUCCESS,
         )
         .await;
+}
+
+// when a tProxy receives a OpenExtendedMiningChannelSuccess with a bad extranonce_size
+// we expect it to shut down gracefully
+#[tokio::test]
+async fn tproxy_refuses_bad_extranonce_size() {
+    let (_tp, tp_addr) = start_template_provider(None);
+    let (_pool, pool_addr) = start_pool(Some(tp_addr)).await;
+
+    let message_replacement = AnyMessage::Mining(Mining::OpenExtendedMiningChannelSuccess(
+        OpenExtendedMiningChannelSuccess {
+            request_id: 0,
+            channel_id: 1,
+            target: [
+                112, 123, 89, 188, 221, 164, 162, 167, 139, 39, 104, 137, 2, 111, 185, 17, 165, 85,
+                33, 115, 67, 45, 129, 197, 134, 103, 128, 151, 59, 19, 0, 0,
+            ]
+            .into(),
+            extranonce_prefix: vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+                .try_into()
+                .unwrap(),
+            extranonce_size: 44, // bad extranonce size
+        },
+    ));
+    let replace_message = ReplaceMessage::new(
+        MessageDirection::ToDownstream,
+        MESSAGE_TYPE_OPEN_EXTENDED_MINING_CHANNEL_SUCCES,
+        message_replacement,
+    );
+
+    // this sniffer will replace OpenExtendedMiningChannelSuccess with a bad extranonce size
+    let (sniffer, sniffer_addr) =
+        start_sniffer("0".to_string(), pool_addr, false, Some(replace_message.into())).await;
+
+    let (_, tproxy_addr) = start_sv2_translator(sniffer_addr).await;
+
+    // make sure tProxy shut down (expected behavior)
+    // we only assert that the listening port is now available
+    assert!(tokio::net::TcpListener::bind(tproxy_addr).await.is_ok());
 }
