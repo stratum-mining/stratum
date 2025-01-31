e7d6c7b72b25867780f3bce625f87c8da468f531
plebhash <plebhash@proton.me>
2025-01-31 09:20:51 -0300

    [MG repo split] use MG from new repo..
    instead of the local MG codebase

--- test/message-generator/test/jds-do-not-fail-on-wrong-tsdatasucc/jds-do-not-fail-on-wrong-tsdatasucc.sh
+++ test/message-generator/test/jds-do-not-fail-on-wrong-tsdatasucc/jds-do-not-fail-on-wrong-tsdatasucc.sh
@@ -1,9 +1,10 @@
-cd roles
-cargo llvm-cov --no-report -p jd_server
+if [ ! -d "utils/stratum-message-generator/" ]; then
+  cd utils
+  git clone https://github.com/stratum-mining/stratum-message-generator
+  cd ..
+fi
 
-cd ../utils/message-generator/
-cargo build
-
+cd utils/stratum-message-generator/
 RUST_LOG=debug cargo run ../../test/message-generator/test/jds-do-not-fail-on-wrong-tsdatasucc/jds-do-not-fail-on-wrong-tsdatasucc.json || { echo 'mg test failed' ; exit 1; }
 
 sleep 10
