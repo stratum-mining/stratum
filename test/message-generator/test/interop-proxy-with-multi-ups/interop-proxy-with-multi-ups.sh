e48f0306b78f4707f0a188c7716df22381579f63
plebhash <plebhash@proton.me>
2025-01-31 09:20:51 -0300

    [MG repo split] use MG from new repo..
    instead of the local MG codebase

--- test/message-generator/test/interop-proxy-with-multi-ups/interop-proxy-with-multi-ups.sh
+++ test/message-generator/test/interop-proxy-with-multi-ups/interop-proxy-with-multi-ups.sh
@@ -1,11 +1,10 @@
-cd roles
-cargo llvm-cov --no-report -p pool_sv2
-cargo llvm-cov --no-report -p mining_proxy_sv2
-cargo build -p mining-device
+if [ ! -d "utils/stratum-message-generator/" ]; then
+  cd utils
+  git clone https://github.com/stratum-mining/stratum-message-generator
+  cd ..
+fi
 
-cd ../utils/message-generator/
-cargo build
-
+cd utils/stratum-message-generator/
 RUST_LOG=debug cargo run ../../test/message-generator/test/interop-proxy-with-multi-ups/interop-proxy-with-multi-ups.json || { echo 'mg test failed' ; exit 1; }
 
 sleep 10
