ebc95904259af6eab6c3c6a180d0209c04f8e4d9
plebhash <plebhash@proton.me>
2025-02-01 10:17:28 -0300

    cleanup async_std from network_helpers_sv2

--- roles/roles-utils/network-helpers/src/lib.rs
+++ roles/roles-utils/network-helpers/src/lib.rs
@@ -1,13 +1,4 @@
-#[cfg(feature = "async_std")]
-mod noise_connection_async_std;
-#[cfg(feature = "async_std")]
-mod plain_connection_async_std;
 use binary_sv2::{Deserialize, GetSize, Serialize};
-#[cfg(feature = "async_std")]
-pub use noise_connection_async_std::{connect, listen, Connection};
-#[cfg(feature = "async_std")]
-pub use plain_connection_async_std::{plain_connect, plain_listen, PlainConnection};
-
 #[cfg(feature = "tokio")]
 pub mod noise_connection_tokio;
 #[cfg(feature = "tokio")]
