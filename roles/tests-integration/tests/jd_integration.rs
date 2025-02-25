cb313f35d945eefc0ba5be52c77352db50c31785
jbesraa <jbesraa@gmail.com>
2025-02-25 16:13:31 +0200

    Test JD Client connection with TP
    The following test assert that JDC and TP will exchange
    `SetupConnection` messages successfully upon intialization.

--- roles/tests-integration/tests/jd_integration.rs
+++ roles/tests-integration/tests/jd_integration.rs
@@ -4,9 +4,10 @@
 //
 // Note that it is enough to call `start_tracing()` once in the test suite to enable tracing for
 // all tests. This is because tracing is a global setting.
+use const_sv2::{MESSAGE_TYPE_SETUP_CONNECTION, MESSAGE_TYPE_SETUP_CONNECTION_SUCCESS};
 use integration_tests_sv2::*;
-
 use roles_logic_sv2::parsers::{CommonMessages, PoolMessages};
+use sniffer::MessageDirection;
 
 // This test verifies that jd-server does not exit when a connected jd-client shuts down.
 //
@@ -25,4 +26,30 @@ async fn jds_should_not_panic_if_jdc_shutsdown() {
     let (sniffer, sniffer_addr) = start_sniffer("0".to_string(), jds_addr, false, None).await;
     let (_jdc_1, _jdc_addr_1) = start_jdc(pool_addr, tp_addr, sniffer_addr).await;
     assert_common_message!(sniffer.next_message_from_downstream(), SetupConnection);
+}
+
+// This test verifies that jd-client exchange SetupConnection messages with a Template Provider.
+//
+// Note that jd-client starts to exchange messages with the Template Provider after it has accepted
+// a downstream connection.
+#[tokio::test]
+async fn jdc_tp_success_setup() {
+    let (tp, tp_addr) = start_template_provider(None);
+    let (_pool, pool_addr) = start_pool(Some(tp_addr)).await;
+    let (_jds, jds_addr) = start_jds(tp.rpc_info()).await;
+    let (tp_jdc_sniffer, tp_jdc_sniffer_addr) =
+        start_sniffer("0".to_string(), tp_addr, false, None).await;
+    let (_jdc, jdc_addr) = start_jdc(pool_addr, tp_jdc_sniffer_addr, jds_addr).await;
+    // This is needed because jd-client waits for a downstream connection before it starts
+    // exchanging messages with the Template Provider.
+    start_sv2_translator(jdc_addr).await;
+    tp_jdc_sniffer
+        .wait_for_message_type(MessageDirection::ToUpstream, MESSAGE_TYPE_SETUP_CONNECTION)
+        .await;
+    tp_jdc_sniffer
+        .wait_for_message_type(
+            MessageDirection::ToDownstream,
+            MESSAGE_TYPE_SETUP_CONNECTION_SUCCESS,
+        )
+        .await;
 }
