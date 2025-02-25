1dec011c7a1ae225e0bac955e4ea6d213377febf
jbesraa <jbesraa@gmail.com>
2025-02-25 09:50:46 +0200

    Document `JobDeclaratorServerConfig`

--- roles/jd-server/src/lib/config.rs
+++ roles/jd-server/src/lib/config.rs
@@ -5,6 +5,10 @@ use serde::Deserialize;
 use std::{convert::TryInto, time::Duration};
 use stratum_common::bitcoin::{Amount, TxOut};
 
+/// Represents the configuration of a Job Declarator Server.
+///
+/// Job Declarator Server connects through RPC to a Bitcoin core node to get the mempool updates.
+/// And it also listens for the connections from the Job Declarator Clients.
 #[derive(Debug, serde::Deserialize, Clone)]
 pub struct JobDeclaratorServerConfig {
     #[serde(default = "default_true")]
@@ -23,6 +27,7 @@ pub struct JobDeclaratorServerConfig {
 }
 
 impl JobDeclaratorServerConfig {
+    /// Creates a new instance of [`JobDeclaratorServerConfig`].
     pub fn new(
         listen_jd_address: String,
         authority_public_key: Secp256k1PublicKey,
@@ -47,54 +52,67 @@ impl JobDeclaratorServerConfig {
         }
     }
 
+    /// Returns the listening address of the Job Declarator Server.
     pub fn listen_jd_address(&self) -> &str {
         &self.listen_jd_address
     }
 
+    /// Returns the public key of the authority.
     pub fn authority_public_key(&self) -> &Secp256k1PublicKey {
         &self.authority_public_key
     }
 
+    /// Returns the secret key of the authority.
     pub fn authority_secret_key(&self) -> &Secp256k1SecretKey {
         &self.authority_secret_key
     }
 
+    /// Returns the URL of the core RPC.
     pub fn core_rpc_url(&self) -> &str {
         &self.core_rpc_url
     }
 
+    /// Returns the port of the core RPC.
     pub fn core_rpc_port(&self) -> u16 {
         self.core_rpc_port
     }
 
+    /// Returns the user of the core RPC.
     pub fn core_rpc_user(&self) -> &str {
         &self.core_rpc_user
     }
 
+    /// Returns the password of the core RPC.
     pub fn core_rpc_pass(&self) -> &str {
         &self.core_rpc_pass
     }
 
+    /// Returns the coinbase outputs.
     pub fn coinbase_outputs(&self) -> &Vec<CoinbaseOutput> {
         &self.coinbase_outputs
     }
 
+    /// Returns the certificate validity in seconds.
     pub fn cert_validity_sec(&self) -> u64 {
         self.cert_validity_sec
     }
 
+    /// Returns whether async mining is allowed.
     pub fn async_mining_allowed(&self) -> bool {
         self.async_mining_allowed
     }
 
+    /// Returns the mempool update interval.
     pub fn mempool_update_interval(&self) -> Duration {
         self.mempool_update_interval
     }
 
+    /// Sets the listening address of Bitcoin core RPC.
     pub fn set_core_rpc_url(&mut self, url: String) {
         self.core_rpc_url = url;
     }
 
+    /// Sets coinbase outputs.
     pub fn set_coinbase_outputs(&mut self, outputs: Vec<CoinbaseOutput>) {
         self.coinbase_outputs = outputs;
     }
