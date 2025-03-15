a617f2f3cec255f8b0808ddeb1200d5aa2cc66c3
bit-aloo <sshourya17@gmail.com>
2025-03-15 20:46:29 +0530

    add shutdown flag

--- roles/test-utils/mining-device-sv1/src/main.rs
+++ roles/test-utils/mining-device-sv1/src/main.rs
@@ -1,7 +1,11 @@
 pub(crate) mod client;
 pub(crate) mod job;
 pub(crate) mod miner;
-use std::{net::SocketAddr, str::FromStr};
+use std::{
+    net::SocketAddr,
+    str::FromStr,
+    sync::{atomic::AtomicBool, Arc},
+};
 
 pub(crate) use client::Client;
 
@@ -9,12 +13,14 @@ pub(crate) use client::Client;
 async fn main() {
     tracing_subscriber::fmt().init();
 
+    let shutdown = Arc::new(AtomicBool::new(false));
     const ADDR: &str = "127.0.0.1:34255";
     Client::connect(
         80,
         SocketAddr::from_str(ADDR).expect("Invalid upstream address"),
         false,
         None,
+        shutdown,
     )
     .await
 }
