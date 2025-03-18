36671de4a63e095b67efa0c347cb8176fff09b68
bit-aloo <sshourya17@gmail.com>
2025-03-18 21:33:20 +0530

    add jds-receive-solution-while-processing-declared-job IT

--- test/integration-tests/tests/jd_integration.rs
+++ test/integration-tests/tests/jd_integration.rs
@@ -1,14 +1,20 @@
 use std::time::Duration;
 
+use binary_sv2::{B032, U256};
 // This file contains integration tests for the `JDC/S` module.
 //
 // `JDC/S` are modules that implements the Job Decleration roles in the Stratum V2 protocol.
 use const_sv2::{
     MESSAGE_TYPE_ALLOCATE_MINING_JOB_TOKEN, MESSAGE_TYPE_ALLOCATE_MINING_JOB_TOKEN_SUCCESS,
-    MESSAGE_TYPE_SETUP_CONNECTION, MESSAGE_TYPE_SETUP_CONNECTION_SUCCESS,
+    MESSAGE_TYPE_DECLARE_MINING_JOB, MESSAGE_TYPE_PROVIDE_MISSING_TRANSACTIONS,
+    MESSAGE_TYPE_PROVIDE_MISSING_TRANSACTIONS_SUCCESS, MESSAGE_TYPE_SETUP_CONNECTION,
+    MESSAGE_TYPE_SETUP_CONNECTION_SUCCESS, MESSAGE_TYPE_SUBMIT_SOLUTION_JD,
 };
-use integration_tests_sv2::{sniffer::MessageDirection, *};
-use roles_logic_sv2::parsers::AnyMessage;
+use integration_tests_sv2::{
+    sniffer::{MessageDirection, ReplaceMessage},
+    *,
+};
+use roles_logic_sv2::{job_declaration_sv2::SubmitSolutionJd, parsers::AnyMessage};
 
 use roles_logic_sv2::parsers::CommonMessages;
 
@@ -107,4 +113,111 @@ async fn jdc_does_not_stackoverflow_when_no_token() {
     // This test verifies that JDC now blocks/yields correctly instead of infinitely recursing.
     tokio::time::sleep(Duration::from_secs(3)).await;
     assert!(tokio::net::TcpListener::bind(jdc_addr).await.is_err());
+}
+
+// This test verifies that JDS does not exit when it receives a `SubmitSolution`
+// while still expecting a `ProvideMissingTransactionsSuccess`.
+//
+// It is performing the verification by connecting to JDS after the message exchange
+// to check whether it remains alive.
+#[tokio::test]
+async fn jds_receive_solution_while_processing_declared_job_test() {
+    start_tracing();
+    let (tp_1, tp_addr_1) = start_template_provider(None);
+
+    let (tp_2, tp_addr_2) = start_template_provider(None);
+
+    let (_pool, pool_addr) = start_pool(Some(tp_addr_1)).await;
+    let (_jds, jds_addr) = start_jds(tp_1.rpc_info()).await;
+
+    // Dummy data to construct a SubmitSolution message
+    let prev_hash = U256::Owned(vec![
+        184, 103, 138, 88, 153, 105, 236, 29, 123, 246, 107, 203, 1, 33, 10, 122, 188, 139, 218,
+        141, 62, 177, 158, 101, 125, 92, 214, 150, 199, 220, 29, 8,
+    ]);
+
+    let extranonce = B032::Owned(vec![
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0,
+        0, 0,
+    ]);
+
+    let submit_solution_replace = ReplaceMessage::new(
+        MessageDirection::ToUpstream,
+        MESSAGE_TYPE_PROVIDE_MISSING_TRANSACTIONS_SUCCESS,
+        AnyMessage::JobDeclaration(roles_logic_sv2::parsers::JobDeclaration::SubmitSolution(
+            SubmitSolutionJd {
+                ntime: 0,
+                nbits: 0,
+                nonce: 0,
+                version: 0,
+                prev_hash,
+                extranonce,
+            },
+        )),
+    );
+
+    // This sniffer sits between `jds` and `jdc`, replacing `ProvideMissingTransactionSuccess`
+    // with `SubmitSolution`.
+    let (sniffer_a, sniffer_a_addr) = start_sniffer(
+        "A".to_string(),
+        jds_addr,
+        false,
+        Some(submit_solution_replace.into()),
+    )
+    .await;
+
+    let (_jdc, jdc_addr) = start_jdc(&[(pool_addr, sniffer_a_addr)], tp_addr_2).await;
+
+    start_sv2_translator(jdc_addr).await;
+
+    assert!(tp_2.fund_wallet().is_ok());
+    assert!(tp_2.create_mempool_transaction().is_ok());
+
+    sniffer_a
+        .wait_for_message_type(MessageDirection::ToUpstream, MESSAGE_TYPE_SETUP_CONNECTION)
+        .await;
+
+    sniffer_a
+        .wait_for_message_type(
+            MessageDirection::ToDownstream,
+            MESSAGE_TYPE_SETUP_CONNECTION_SUCCESS,
+        )
+        .await;
+
+    sniffer_a
+        .wait_for_message_type(
+            MessageDirection::ToUpstream,
+            MESSAGE_TYPE_ALLOCATE_MINING_JOB_TOKEN,
+        )
+        .await;
+
+    sniffer_a
+        .wait_for_message_type(
+            MessageDirection::ToDownstream,
+            MESSAGE_TYPE_ALLOCATE_MINING_JOB_TOKEN_SUCCESS,
+        )
+        .await;
+
+    sniffer_a
+        .wait_for_message_type(
+            MessageDirection::ToUpstream,
+            MESSAGE_TYPE_DECLARE_MINING_JOB,
+        )
+        .await;
+
+    sniffer_a
+        .wait_for_message_type(
+            MessageDirection::ToDownstream,
+            MESSAGE_TYPE_PROVIDE_MISSING_TRANSACTIONS,
+        )
+        .await;
+
+    sniffer_a
+        .wait_for_message_type(
+            MessageDirection::ToUpstream,
+            MESSAGE_TYPE_SUBMIT_SOLUTION_JD,
+        )
+        .await;
+
+    assert!(tokio::net::TcpListener::bind(jds_addr).await.is_err());
 }
