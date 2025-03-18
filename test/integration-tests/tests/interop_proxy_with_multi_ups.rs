73a052c6ca0834915a3449bf9af67c542ac5e065
bit-aloo <sshourya17@gmail.com>
2025-03-18 10:56:53 +0530

    Add interop_proxy_with_multiple_ups

---
+++ test/integration-tests/tests/interop_proxy_with_multi_ups.rs
@@ -0,0 +1,15 @@
+use integration_tests_sv2::*;
+
+#[tokio::test]
+async fn test_interop_proxy_with_multi_ups() {
+    start_tracing();
+    let (_, tp_addr) = start_template_provider(None);
+    let (_pool_1, pool_addr_1) = start_pool(Some(tp_addr)).await;
+    let (_pool_2, pool_addr_2) = start_pool(Some(tp_addr)).await;
+    let mining_proxy_sv2_addr = start_mining_sv2_proxy(&[pool_addr_1, pool_addr_2]).await;
+    let _ = start_mining_device_sv1(mining_proxy_sv2_addr, false, None).await;
+    let _ = start_mining_device_sv1(mining_proxy_sv2_addr, false, None).await;
+
+    // Need a way to kill pool. And then test if mining_proxy_sv2_addr still binds or not.
+    // Need a way to kill MD. And then test if mining_proxy_sv2_addr still binds or not.
+}
