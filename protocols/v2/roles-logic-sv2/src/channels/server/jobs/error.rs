efa21d4ade8f87120b597a10d85044d0852be142
plebhash <plebhash@proton.me>
2025-06-06 15:10:44 -0300

    implement channels::server::jobs::ExtendedJob::into_custom_job

--- protocols/v2/roles-logic-sv2/src/channels/server/jobs/error.rs
+++ protocols/v2/roles-logic-sv2/src/channels/server/jobs/error.rs
@@ -1,6 +1,10 @@
 pub enum ExtendedJobError {
-    CoinbaseOutputsSumOverflow,
-    InvalidCoinbaseOutputsSum,
+    FailedToDeserializeCoinbase,
+    CoinbaseInputCountMismatch,
+    FailedToSerializeCoinbaseOutputs,
+    FailedToSerializeCoinbasePrefix,
+    FutureJobNotAllowed,
+    InvalidMinNTime,
 }
 
 pub enum StandardJobError {}
