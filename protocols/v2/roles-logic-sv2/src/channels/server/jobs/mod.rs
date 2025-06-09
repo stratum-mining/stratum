5c06efc722ffb30827e66102edba8078cc29d7f8
plebhash <plebhash@proton.me>
2025-06-09 20:05:54 -0300

    test_custom_job_creation_flow

--- protocols/v2/roles-logic-sv2/src/channels/server/jobs/mod.rs
+++ protocols/v2/roles-logic-sv2/src/channels/server/jobs/mod.rs
@@ -6,7 +6,7 @@ pub mod standard;
 use mining_sv2::SetCustomMiningJob;
 use template_distribution_sv2::NewTemplate;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum JobOrigin<'a> {
     NewTemplate(NewTemplate<'a>),
     SetCustomMiningJob(SetCustomMiningJob<'a>),
