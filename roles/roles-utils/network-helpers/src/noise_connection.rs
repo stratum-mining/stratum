335ce50a708a14c43325cac4ca59ec9bc7caa7ea
bit-aloo <sshourya17@gmail.com>
2025-06-18 18:25:13 +0530

    Restructure the `noise_connection` Architecture
    This commit simplifies the previously convoluted setup used
    to make the noise connection work. We had been facing lingering
    (ghost) tasks in our roles for quite some time, which originated
    within the network helpers, particularly in the noise connection logic.
    Previously, the handshake was performed in a somewhat blocking or
    compute-intensive manner, which could have contributed to the ghost
    task issue and made the flow difficult to reason about. In this
    revision, we introduce a much simpler and more intuitive flow: the
    handshake is now fully completed first, followed by spawning tasks
    for handling subsequent message processing for each role. This is a
    significant shift from the earlier structure, where tasks were spawned
    immediately and the handshake messages were exchanged later, making
    the sequence complex and prone to starvation. We also previously
    wrapped the state in synchronous primitives to share it across streams,
    which required locking even though encryption and decryption in
    the Noise protocol are completely independent operations. As noted
    in the previous commit, we now clone the state, allowing each stream
    to use its own dedicated encryptor or decryptor without interfering
    with the other. These were the key issues likely contributing to the
    ghost tasks. This commit simplifies the architecture and makes the
    entire flow much more intuitive and easier to maintain.

--- roles/roles-utils/network-helpers/src/noise_connection.rs
+++ roles/roles-utils/network-helpers/src/noise_connection.rs
@@ -1,13 +1,18 @@
 use crate::Error;
 use async_channel::{unbounded, Receiver, Sender};
 use binary_sv2::{Deserialize, GetSize, Serialize};
-use codec_sv2::{HandshakeRole, StandardEitherFrame, StandardNoiseDecoder};
-use futures::lock::Mutex;
-use std::sync::Arc;
+use codec_sv2::{
+    noise_sv2::{ELLSWIFT_ENCODING_SIZE, INITIATOR_EXPECTED_HANDSHAKE_MESSAGE_SIZE},
+    HandShakeFrame, HandshakeRole, StandardEitherFrame, StandardNoiseDecoder, State,
+};
+use std::convert::TryInto;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpStream,
-    select, task,
+    net::{
+        tcp::{OwnedReadHalf, OwnedWriteHalf},
+        TcpStream,
+    },
+    task,
 };
 use tracing::{debug, error};
 
@@ -16,23 +21,34 @@ pub struct Connection {
     pub state: codec_sv2::State,
 }
 
-impl crate::SetState for Connection {
-    async fn set_state(self_: Arc<Mutex<Self>>, state: codec_sv2::State) {
-        loop {
-            if crate::HANDSHAKE_READY.load(std::sync::atomic::Ordering::SeqCst) {
-                if let Some(mut connection) = self_.try_lock() {
-                    connection.state = state;
-                    crate::TRANSPORT_READY.store(true, std::sync::atomic::Ordering::Relaxed);
-                    break;
-                };
-            }
-            task::yield_now().await;
-        }
-    }
+async fn send_message<'a, Message: Serialize + Deserialize<'a> + GetSize + Send + 'static>(
+    writer: &mut OwnedWriteHalf,
+    msg: StandardEitherFrame<Message>,
+    state: &mut State,
+    encoder: &mut codec_sv2::NoiseEncoder<Message>,
+) -> Result<(), Error> {
+    let buffer = encoder.encode(msg, state)?;
+    writer
+        .write_all(buffer.as_ref())
+        .await
+        .map_err(|_| Error::SocketClosed)?;
+    Ok(())
+}
+
+async fn receive_message<'a, Message: Serialize + Deserialize<'a> + GetSize + Send + 'static>(
+    reader: &mut OwnedReadHalf,
+    state: &mut State,
+    decoder: &mut StandardNoiseDecoder<Message>,
+) -> Result<StandardEitherFrame<Message>, Error> {
+    let writable = decoder.writable();
+    reader
+        .read_exact(writable)
+        .await
+        .map_err(|_| Error::SocketClosed)?;
+    decoder.next_frame(state).map_err(Error::CodecError)
 }
 
 impl Connection {
-    #[allow(clippy::new_ret_no_self)]
     pub async fn new<'a, Message: Serialize + Deserialize<'a> + GetSize + Send + 'static>(
         stream: TcpStream,
         role: HandshakeRole,
@@ -44,138 +60,143 @@ impl Connection {
         Error,
     > {
         let address = stream.peer_addr().map_err(|_| Error::SocketClosed)?;
-
         let (mut reader, mut writer) = stream.into_split();
+        let mut decoder = StandardNoiseDecoder::<Message>::new();
+        let mut encoder = codec_sv2::NoiseEncoder::<Message>::new();
+        let mut state = codec_sv2::State::initialized(role.clone());
 
-        let (sender_incoming, receiver_incoming): (
-            Sender<StandardEitherFrame<Message>>,
-            Receiver<StandardEitherFrame<Message>>,
-        ) = unbounded();
-        let (sender_outgoing, receiver_outgoing): (
-            Sender<StandardEitherFrame<Message>>,
-            Receiver<StandardEitherFrame<Message>>,
-        ) = unbounded();
-
-        let state = codec_sv2::State::not_initialized(&role);
-
-        let connection = Arc::new(Mutex::new(Self { state }));
-
-        let cloned1 = connection.clone();
-        let cloned2 = connection.clone();
-
-        task::spawn(async move {
-            select!(
-              _ = tokio::signal::ctrl_c() => { },
-              _ = async {
-                let mut decoder = StandardNoiseDecoder::<Message>::new();
-                loop {
-                  let writable = decoder.writable();
-                  match reader.read_exact(writable).await {
-                    Ok(_) => {
-                      let mut connection = cloned1.lock().await;
-                      let decoded = decoder.next_frame(&mut connection.state);
-                      drop(connection);
-                      match decoded {
-                        Ok(x) => {
-                          if sender_incoming.send(x).await.is_err() {
-                            error!("Shutting down noise stream reader!");
-                            break;
-                          }
-                        }
-                        Err(e) => {
-                          if let codec_sv2::Error::MissingBytes(_) = e {
-                          } else {
-                            error!("Shutting down noise stream reader! {:#?}", e);
-                            sender_incoming.close();
-                            break;
-                          }
-                        }
-                      }
-                    }
-                    Err(e) => {
-                      error!(
-                        "Disconnected from client while reading : {} - {}",
-                        e, &address
-                      );
-                      sender_incoming.close();
-                      break;
-                    }
-                  }
-                }
-              } => {}
-            );
-        });
-
-        let receiver_outgoing_cloned = receiver_outgoing.clone();
-        task::spawn(async move {
-            select!(
-              _ = tokio::signal::ctrl_c() => { },
-              _ = async {
-                let mut encoder = codec_sv2::NoiseEncoder::<Message>::new();
-                loop {
-                  let received = receiver_outgoing_cloned.recv().await;
-
-                  match received {
-                    Ok(frame) => {
-                      let mut connection = cloned2.lock().await;
-                      let b = encoder.encode(frame, &mut connection.state).unwrap();
-                      drop(connection);
-                      let b = b.as_ref();
-                      match (writer).write_all(b).await {
-                        Ok(_) => (),
-                        Err(e) => {
-                          let _ = writer.shutdown().await;
-                          // Just fail and force to reinitialize everything
-                          error!(
-                            "Disconnecting from client due to error writing: {} - {}",
-                            e, &address
-                          );
-                          task::yield_now().await;
-                          break;
-                        }
-                      }
-                    }
-                    Err(e) => {
-                      // Just fail and force to reinitialize everything
-                      let _ = writer.shutdown().await;
-                      error!(
-                        "Disconnecting from client due to error receiving: {} - {}",
-                        e, &address
-                      );
-                      task::yield_now().await;
-                      break;
-                    }
-                  };
-                  crate::HANDSHAKE_READY.store(true, std::sync::atomic::Ordering::Relaxed);
-                }
-              } => {}
-            );
-        });
-
-        // DO THE NOISE HANDSHAKE
+        // Handshake Phase
         match role {
             HandshakeRole::Initiator(_) => {
-                debug!("Initializing as downstream for - {}", &address);
-                crate::initialize_as_downstream(
-                    connection.clone(),
-                    role,
-                    sender_outgoing.clone(),
-                    receiver_incoming.clone(),
-                )
-                .await?
+                debug!("Initializing as downstream for {}", address);
+                let mut responder_state = codec_sv2::State::not_initialized(&role);
+                let first_msg = state.step_0()?;
+                send_message(&mut writer, first_msg.into(), &mut state, &mut encoder).await?;
+                debug!("First handshake message sent");
+
+                loop {
+                    match receive_message(&mut reader, &mut responder_state, &mut decoder).await {
+                        Ok(second_msg) => {
+                            debug!("Second handshake message received");
+                            let handshake_frame: HandShakeFrame = second_msg
+                                .try_into()
+                                .map_err(|_| Error::HandshakeRemoteInvalidMessage)?;
+                            let payload: [u8; INITIATOR_EXPECTED_HANDSHAKE_MESSAGE_SIZE] =
+                                handshake_frame
+                                    .get_payload_when_handshaking()
+                                    .try_into()
+                                    .map_err(|_| Error::HandshakeRemoteInvalidMessage)?;
+                            let transport_state = state.step_2(payload)?;
+                            state = transport_state;
+                            break;
+                        }
+                        Err(Error::CodecError(codec_sv2::Error::MissingBytes(_))) => {
+                            debug!("Waiting for more bytes during handshake");
+                        }
+                        Err(e) => {
+                            error!("Handshake failed with upstream: {:?}", e);
+                            return Err(e);
+                        }
+                    }
+                }
             }
             HandshakeRole::Responder(_) => {
-                debug!("Initializing as upstream for - {}", &address);
-                crate::initialize_as_upstream(
-                    connection.clone(),
-                    role,
-                    sender_outgoing.clone(),
-                    receiver_incoming.clone(),
-                )
-                .await?
+                debug!("Initializing as upstream for {}", address);
+                let mut initiator_state = codec_sv2::State::not_initialized(&role);
+
+                loop {
+                    match receive_message(&mut reader, &mut initiator_state, &mut decoder).await {
+                        Ok(first_msg) => {
+                            debug!("First handshake message received");
+                            let handshake_frame: HandShakeFrame = first_msg
+                                .try_into()
+                                .map_err(|_| Error::HandshakeRemoteInvalidMessage)?;
+                            let payload: [u8; ELLSWIFT_ENCODING_SIZE] = handshake_frame
+                                .get_payload_when_handshaking()
+                                .try_into()
+                                .map_err(|_| Error::HandshakeRemoteInvalidMessage)?;
+                            let (second_msg, transport_state) = state.step_1(payload)?;
+                            send_message(&mut writer, second_msg.into(), &mut state, &mut encoder)
+                                .await?;
+                            debug!("Second handshake message sent");
+                            state = transport_state;
+                            break;
+                        }
+                        Err(Error::CodecError(codec_sv2::Error::MissingBytes(_))) => {
+                            debug!("Waiting for more bytes during handshake");
+                        }
+                        Err(e) => {
+                            error!("Handshake failed with downstream: {:?}", e);
+                            return Err(e);
+                        }
+                    }
+                }
             }
         };
-        debug!("Noise handshake complete - {}", &address);
+
+        debug!("Handshake completed with state: {:?}", state);
+
+        let (sender_incoming, receiver_incoming) = unbounded();
+        let (sender_outgoing, receiver_outgoing) = unbounded();
+
+        // Spawn Reader
+        let read_state = state.clone();
+        Self::spawn_reader(reader, read_state, address, sender_incoming.clone());
+
+        // Spawn Writer
+        let write_state = state;
+        Self::spawn_writer(writer, write_state, address, receiver_outgoing.clone());
+
         Ok((receiver_incoming, sender_outgoing))
+    }
+
+    fn spawn_reader<'a, Message: Serialize + Deserialize<'a> + GetSize + Send + 'static>(
+        mut reader: OwnedReadHalf,
+        mut reader_state: State,
+        address: std::net::SocketAddr,
+        sender_incoming: Sender<StandardEitherFrame<Message>>,
+    ) -> task::JoinHandle<()> {
+        task::spawn(async move {
+            let mut decoder = StandardNoiseDecoder::<Message>::new();
+            loop {
+                match receive_message(&mut reader, &mut reader_state, &mut decoder).await {
+                    Ok(frame) => {
+                        if sender_incoming.send(frame).await.is_err() {
+                            error!("Shutting down reader for {}", address);
+                            break;
+                        }
+                    }
+                    Err(Error::CodecError(codec_sv2::Error::MissingBytes(_))) => {
+                        debug!("Waiting for more bytes while reading stream");
+                    }
+                    Err(e) => {
+                        error!("Reader shutting down due to error: {:?}", e);
+                        sender_incoming.close();
+                        break;
+                    }
+                }
+            }
+        })
+    }
+
+    fn spawn_writer<'a, Message: Serialize + Deserialize<'a> + GetSize + Send + 'static>(
+        mut writer: OwnedWriteHalf,
+        mut write_state: State,
+        address: std::net::SocketAddr,
+        receiver_outgoing: Receiver<StandardEitherFrame<Message>>,
+    ) -> task::JoinHandle<()> {
+        task::spawn(async move {
+            let mut encoder = codec_sv2::NoiseEncoder::<Message>::new();
+            while let Ok(frame) = receiver_outgoing.recv().await {
+                if let Err(e) =
+                    send_message(&mut writer, frame, &mut write_state, &mut encoder).await
+                {
+                    error!("Error while writing to client {}: {:?}", address, e);
+                    let _ = writer.shutdown().await;
+                    break;
+                }
+            }
+            let _ = writer.shutdown().await;
+        })
     }
 }
