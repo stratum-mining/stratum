c06a729891ad0ec1f29bfd5e8d929b6b42ea8494
bit-aloo <sshourya17@gmail.com>
2025-06-30 16:29:43 +0530

    combine reader and writer half into a single spawn task

--- roles/roles-utils/network-helpers/src/noise_connection.rs
+++ roles/roles-utils/network-helpers/src/noise_connection.rs
@@ -6,7 +6,7 @@ use codec_sv2::{
     noise_sv2::{ELLSWIFT_ENCODING_SIZE, INITIATOR_EXPECTED_HANDSHAKE_MESSAGE_SIZE},
     HandShakeFrame, HandshakeRole, StandardEitherFrame, StandardNoiseDecoder, State,
 };
-use std::{convert::TryInto, sync::Arc};
+use std::convert::TryInto;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{
@@ -15,7 +15,7 @@ use tokio::{
     },
     task,
 };
-use tracing::{debug, error};
+use tracing::{debug, error, info};
 
 pub struct Connection;
 
@@ -28,6 +28,7 @@ struct ConnectionState<Message> {
 
 impl<Message> ConnectionState<Message> {
     fn close_all(&self) {
+        debug!("Closing all channels");
         self.sender_incoming.close();
         self.receiver_incoming.close();
         self.sender_outgoing.close();
@@ -54,11 +55,16 @@ async fn receive_message<'a, Message: Serialize + Deserialize<'a> + GetSize + Se
     state: &mut State,
     decoder: &mut StandardNoiseDecoder<Message>,
 ) -> Result<StandardEitherFrame<Message>, Error> {
-    let writable = decoder.writable();
+    let mut buf = vec![0u8; decoder.writable_len()];
+    debug!("Bytes to read: {}", buf.len());
+
     reader
-        .read_exact(writable)
+        .read_exact(&mut buf)
         .await
         .map_err(|_| Error::SocketClosed)?;
+
+    debug!("Read complete, copying into decoder buffer");
+    decoder.writable().copy_from_slice(&buf);
     decoder.next_frame(state).map_err(Error::CodecError)
 }
 
@@ -75,6 +81,7 @@ impl Connection {
     > {
         let address = stream.peer_addr().map_err(|_| Error::SocketClosed)?;
         let (mut reader, mut writer) = stream.into_split();
+
         let mut decoder = StandardNoiseDecoder::<Message>::new();
         let mut encoder = codec_sv2::NoiseEncoder::<Message>::new();
         let mut state = codec_sv2::State::initialized(role.clone());
@@ -148,81 +155,84 @@ impl Connection {
             }
         };
 
-        debug!("Handshake completed with state: {:?}", state);
+        error!("Handshake completed with state: {:?}", state);
 
         let (sender_incoming, receiver_incoming) = unbounded();
         let (sender_outgoing, receiver_outgoing) = unbounded();
 
-        let conn_state = Arc::new(ConnectionState {
+        let conn_state = ConnectionState {
             sender_incoming,
             receiver_incoming: receiver_incoming.clone(),
             sender_outgoing: sender_outgoing.clone(),
             receiver_outgoing,
-        });
-
-        // Spawn Reader
-        let read_state = state.clone();
-        Self::spawn_reader(reader, read_state, address, conn_state.clone());
-
-        // Spawn Writer
-        let write_state = state;
-        Self::spawn_writer(writer, write_state, address, conn_state);
+        };
+        Self::spawn_connection_loop(reader, writer, state, address, conn_state);
 
         Ok((receiver_incoming, sender_outgoing))
     }
 
-    fn spawn_reader<'a, Message: Serialize + Deserialize<'a> + GetSize + Send + 'static>(
+    fn spawn_connection_loop<
+        'a,
+        Message: Serialize + Deserialize<'a> + GetSize + Send + 'static,
+    >(
         mut reader: OwnedReadHalf,
-        mut reader_state: State,
+        mut writer: OwnedWriteHalf,
+        state: State,
         address: std::net::SocketAddr,
-        conn_state: Arc<ConnectionState<Message>>,
+        conn_state: ConnectionState<Message>,
     ) -> task::JoinHandle<()> {
         let sender_incoming = conn_state.sender_incoming.clone();
+        let receiver_outgoing = conn_state.receiver_outgoing.clone();
+
         task::spawn(async move {
             let mut decoder = StandardNoiseDecoder::<Message>::new();
+            let mut encoder = codec_sv2::NoiseEncoder::<Message>::new();
+            let mut read_state = state.clone();
+            let mut write_state = state;
+
             loop {
-                match receive_message(&mut reader, &mut reader_state, &mut decoder).await {
-                    Ok(frame) => {
-                        if sender_incoming.send(frame).await.is_err() {
-                            error!("Shutting down reader for {}", address);
-                            conn_state.close_all();
-                            break;
+                tokio::select! {
+                    // Incoming message from network
+                    res = receive_message(&mut reader, &mut read_state, &mut decoder) => {
+                        match res {
+                            Ok(frame) => {
+                                if sender_incoming.send(frame).await.is_err() {
+                                    debug!("Receiver dropped for {}", address);
+                                    break;
+                                }
+                            }
+                            Err(Error::CodecError(codec_sv2::Error::MissingBytes(_))) => {
+                                debug!("Waiting for more stream bytes...");
+                            }
+                            Err(e) => {
+                                error!("Read error for {}: {:?}, shutting down", address, e);
+                                break;
+                            }
                         }
                     }
-                    Err(Error::CodecError(codec_sv2::Error::MissingBytes(_))) => {
-                        debug!("Waiting for more bytes while reading stream");
-                    }
-                    Err(e) => {
-                        error!("Reader shutting down due to error: {:?}", e);
-                        conn_state.close_all();
-                        break;
-                    }
-                }
-            }
-        })
-    }
 
-    fn spawn_writer<'a, Message: Serialize + Deserialize<'a> + GetSize + Send + 'static>(
-        mut writer: OwnedWriteHalf,
-        mut write_state: State,
-        address: std::net::SocketAddr,
-        conn_state: Arc<ConnectionState<Message>>,
-    ) -> task::JoinHandle<()> {
-        let receiver_outgoing = conn_state.receiver_outgoing.clone();
-        task::spawn(async move {
-            let mut encoder = codec_sv2::NoiseEncoder::<Message>::new();
-            while let Ok(frame) = receiver_outgoing.recv().await {
-                if let Err(e) =
-                    send_message(&mut writer, frame, &mut write_state, &mut encoder).await
-                {
-                    error!("Error while writing to client {}: {:?}", address, e);
-                    let _ = writer.shutdown().await;
-                    conn_state.close_all();
-                    break;
+                    // Outgoing message from channel
+                    res = receiver_outgoing.recv() => {
+                        match res {
+                            Ok(frame) => {
+                                if let Err(e) = send_message(&mut writer, frame, &mut write_state, &mut encoder).await {
+                                    error!("Write error for {address}: {e:?}, shutting down");
+                                    break;
+                                }
+                            }
+                            Err(_) => {
+                                debug!("Sender closed for {address}");
+                                break;
+                            }
+                        }
+                    }
                 }
             }
+
             let _ = writer.shutdown().await;
+            drop(reader);
             conn_state.close_all();
+            info!("Connection to {} shut down cleanly", address);
         })
     }
 }
