22ef51a18a2d25cc46f8dc08cf4a217a7d3e218e
Lucas Balieiro <github@lucasbalieiro.dev>
2025-07-03 15:36:28 -0400

    add logging initialization and CLI argument enhancements for Job Declarator Client

--- roles/jd-client/src/args.rs
+++ roles/jd-client/src/args.rs
@@ -22,11 +22,32 @@ pub struct Args {
         default_value = "jdc-config.toml"
     )]
     pub config_path: PathBuf,
+    #[arg(
+        short = 'f',
+        long = "log-file",
+        help = "Path to the log file. If not set, logs will only be written to stdout."
+    )]
+    pub log_file: Option<PathBuf>,
+    #[arg(
+        long = "log-level",
+        help = "Log level (error, warn, info, debug, trace)",
+        default_value = "info"
+    )]
+    pub log_level: String,
+    #[arg(
+        short = 'v',
+        long = "verbose-stdout",
+        help = "If set, logs will also be written to stdout. Requires --log-file (-f).",
+        default_value_t = false,
+        action = clap::ArgAction::SetTrue,
+        requires= "log_file"
+    )]
+    pub verbose_stdout: bool,
 }
 
 /// Process CLI args and load configuration.
 #[allow(clippy::result_large_err)]
-pub fn process_cli_args<'a>() -> ProxyResult<'a, JobDeclaratorClientConfig> {
+pub fn process_cli_args<'a>() -> ProxyResult<'a, (JobDeclaratorClientConfig, Args)> {
     // Parse CLI arguments
     let args = Args::parse();
 
@@ -42,5 +63,5 @@ pub fn process_cli_args<'a>() -> ProxyResult<'a, JobDeclaratorClientConfig> {
 
     // Deserialize settings into JobDeclaratorClientConfig
     let config = settings.try_deserialize::<JobDeclaratorClientConfig>()?;
-    Ok(config)
+    Ok((config, args))
 }
