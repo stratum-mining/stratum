3efa4b8f5a87a74ef207ae2f8b03a28cb80e08e2
Lucas Balieiro <github@lucasbalieiro.dev>
2025-07-28 13:36:32 -0400

    finishes rust docs for `protocols::v2::channels_sv2`

--- protocols/v2/channels-sv2/src/chain_tip.rs
+++ protocols/v2/channels-sv2/src/chain_tip.rs
@@ -3,7 +3,9 @@ use binary_sv2::U256;
 
 /// An abstraction over the chain tip, carrying information from `SetNewPrevHash` messages.
 ///
-/// Used while creating non-future jobs.
+/// Used for:
+/// - creating non-future jobs
+/// - validating shares.
 #[derive(Debug, Clone)]
 pub struct ChainTip {
     prev_hash: U256<'static>,
@@ -12,6 +14,7 @@ pub struct ChainTip {
 }
 
 impl ChainTip {
+    /// Constructs a new `ChainTip` instance.
     pub fn new(prev_hash: U256<'static>, nbits: u32, min_ntime: u32) -> Self {
         Self {
             prev_hash,
@@ -20,14 +23,17 @@ impl ChainTip {
         }
     }
 
+    /// Retrieves the hash of the previous block
     pub fn prev_hash(&self) -> U256<'static> {
         self.prev_hash.clone()
     }
 
+    /// Retrieves the network difficulty for the current block
     pub fn nbits(&self) -> u32 {
         self.nbits
     }
 
+    /// Retrieves the smallest nTime value available for hashing
     pub fn min_ntime(&self) -> u32 {
         self.min_ntime
     }
--- protocols/v2/channels-sv2/src/lib.rs
+++ protocols/v2/channels-sv2/src/lib.rs
@@ -1,3 +1,18 @@
+//! # Stratum V2 Channels
+//!
+//! `channels_sv2` provides primitives and abstractions for Stratum V2 (Sv2) Channels.
+//!
+//! This crate implements the core channel management functionality for both mining clients and
+//! servers, including standard, extended, and group channels, and share accounting mechanisms.
+//!
+//! ## Features
+//!
+//! - Channel primitives for SV2 mining protocol
+//! - Channel management for mining servers and clients
+//! - Standard, extended, and group channel support
+//! - Share accounting
+//! - Job store abstractions
+
 pub mod bip141;
 pub mod chain_tip;
 pub mod client;
