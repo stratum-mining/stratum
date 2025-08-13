269ab79518c2411224a075b8d01059ad66b8d58a
bit-aloo <sshourya17@gmail.com>
2025-08-13 15:59:04 +0530

    add distinction to client and server common methods in common.rs

--- protocols/v2/handlers-sv2/src/common.rs
+++ protocols/v2/handlers-sv2/src/common.rs
@@ -6,12 +6,16 @@ use core::convert::TryInto;
 use parsers_sv2::CommonMessages;
 
 pub trait HandleCommonMessagesFromServerSync {
-    fn handle_common_message(&mut self, message_type: u8, payload: &mut [u8]) -> Result<(), Error> {
+    fn handle_common_message_server(
+        &mut self,
+        message_type: u8,
+        payload: &mut [u8],
+    ) -> Result<(), Error> {
         let parsed: CommonMessages<'_> = (message_type, payload).try_into()?;
-        self.dispatch_common_message(parsed)
+        self.dispatch_common_message_server(parsed)
     }
 
-    fn dispatch_common_message(&mut self, message: CommonMessages<'_>) -> Result<(), Error> {
+    fn dispatch_common_message_server(&mut self, message: CommonMessages<'_>) -> Result<(), Error> {
         match message {
             CommonMessages::SetupConnectionSuccess(msg) => {
                 self.handle_setup_connection_success(msg)
@@ -41,7 +45,7 @@ pub trait HandleCommonMessagesFromServerSync {
 
 #[trait_variant::make(Send)]
 pub trait HandleCommonMessagesFromServerAsync {
-    async fn handle_common_message(
+    async fn handle_common_message_server(
         &mut self,
         message_type: u8,
         payload: &mut [u8],
@@ -49,11 +53,14 @@ pub trait HandleCommonMessagesFromServerAsync {
         let parsed: Result<CommonMessages<'_>, _> = (message_type, payload).try_into();
         async move {
             let parsed = parsed?;
-            self.dispatch_common_message(parsed).await
+            self.dispatch_common_message_server(parsed).await
         }
     }
 
-    async fn dispatch_common_message(&mut self, message: CommonMessages<'_>) -> Result<(), Error> {
+    async fn dispatch_common_message_server(
+        &mut self,
+        message: CommonMessages<'_>,
+    ) -> Result<(), Error> {
         async move {
             match message {
                 CommonMessages::SetupConnectionSuccess(msg) => {
@@ -93,12 +100,16 @@ pub trait HandleCommonMessagesFromServerAsync {
 }
 
 pub trait HandleCommonMessagesFromClientSync {
-    fn handle_common_message(&mut self, message_type: u8, payload: &mut [u8]) -> Result<(), Error> {
+    fn handle_common_message_client(
+        &mut self,
+        message_type: u8,
+        payload: &mut [u8],
+    ) -> Result<(), Error> {
         let parsed: CommonMessages<'_> = (message_type, payload).try_into()?;
-        self.dispatch_common_message(parsed)
+        self.dispatch_common_message_client(parsed)
     }
 
-    fn dispatch_common_message(&mut self, message: CommonMessages<'_>) -> Result<(), Error> {
+    fn dispatch_common_message_client(&mut self, message: CommonMessages<'_>) -> Result<(), Error> {
         match message {
             CommonMessages::SetupConnectionSuccess(_) => Err(Error::UnexpectedMessage(
                 MESSAGE_TYPE_SETUP_CONNECTION_SUCCESS,
@@ -120,7 +131,7 @@ pub trait HandleCommonMessagesFromClientSync {
 
 #[trait_variant::make(Send)]
 pub trait HandleCommonMessagesFromClientAsync {
-    async fn handle_common_message(
+    async fn handle_common_message_client(
         &mut self,
         message_type: u8,
         payload: &mut [u8],
@@ -128,11 +139,14 @@ pub trait HandleCommonMessagesFromClientAsync {
         let parsed: Result<CommonMessages<'_>, _> = (message_type, payload).try_into();
         async move {
             let parsed = parsed?;
-            self.dispatch_common_message(parsed).await
+            self.dispatch_common_message_client(parsed).await
         }
     }
 
-    async fn dispatch_common_message(&mut self, message: CommonMessages<'_>) -> Result<(), Error> {
+    async fn dispatch_common_message_client(
+        &mut self,
+        message: CommonMessages<'_>,
+    ) -> Result<(), Error> {
         async move {
             match message {
                 CommonMessages::SetupConnectionSuccess(_) => Err(Error::UnexpectedMessage(
