251403b90a293df819f89226e4e98586239a3d98
bit-aloo <sshourya17@gmail.com>
2025-09-18 13:41:26 +0530

    remove handler error and add handleError type

--- protocols/v2/handlers-sv2/src/error.rs
+++ protocols/v2/handlers-sv2/src/error.rs
@@ -1,14 +1,5 @@
 use parsers_sv2::ParserError;
-
-#[derive(Debug)]
-pub enum HandlerError {
-    UnexpectedMessage(u8),
-    ParserError(ParserError),
-    External(Box<dyn std::error::Error + Send + Sync>),
-}
-
-impl From<ParserError> for HandlerError {
-    fn from(value: ParserError) -> HandlerError {
-        HandlerError::ParserError(value)
-    }
+pub trait HandlerErrorType {
+    fn unexpected_message(message_type: u8) -> Self;
+    fn parse_error(error: ParserError) -> Self;
 }
