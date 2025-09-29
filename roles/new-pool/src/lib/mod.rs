319c0030ee5e1e96a710a1713b29de5a64c8a95b
bit-aloo <sshourya17@gmail.com>
2025-09-29 19:44:41 +0530

    add subsystem skeleton

--- roles/new-pool/src/lib/mod.rs
+++ roles/new-pool/src/lib/mod.rs
@@ -1,13 +1,15 @@
 use crate::config::PoolConfig;
 
 pub mod config;
-
+pub mod error;
+pub mod template_receiver;
+pub mod downstream;
+pub mod channel_manager;
 
 #[derive(Debug, Clone)]
 pub struct PoolSv2 {
     config: PoolConfig,
 }
-
 
 impl PoolSv2 {
     pub fn new(config: PoolConfig) -> Self {
