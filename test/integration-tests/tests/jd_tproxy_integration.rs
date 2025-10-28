bead6c59bdf66831587b7efbbedc9e9f2df6342b
plebhash <plebhash@proton.me>
2025-10-27 22:01:55 -0300

    cover aggregated tProxy in jd_tproxy_integration.rs

--- test/integration-tests/tests/jd_tproxy_integration.rs
+++ test/integration-tests/tests/jd_tproxy_integration.rs
@@ -2,7 +2,7 @@ use integration_tests_sv2::{interceptor::MessageDirection, template_provider::Di
 use stratum_apps::stratum_core::{common_messages_sv2::*, mining_sv2::*};
 
 #[tokio::test]
-async fn jd_tproxy_integration() {
+async fn jd_non_aggregated_tproxy_integration() {
     start_tracing();
     let (tp, tp_addr) = start_template_provider(None, DifficultyLevel::Low);
     let (_pool, pool_addr) = start_pool(Some(tp_addr)).await;
@@ -10,8 +10,96 @@ async fn jd_tproxy_integration() {
         start_sniffer("0", pool_addr, false, vec![], None);
     let (_jds, jds_addr) = start_jds(tp.rpc_info());
     let (_jdc, jdc_addr) = start_jdc(&[(jdc_pool_sniffer_addr, jds_addr)], tp_addr);
-    let (_translator, tproxy_addr) = start_sv2_translator(jdc_addr, false).await;
+    let (tproxy_jdc_sniffer, tproxy_jdc_sniffer_addr) =
+        start_sniffer("1", jdc_addr, false, vec![], None);
+    let (_translator, tproxy_addr) = start_sv2_translator(tproxy_jdc_sniffer_addr, false).await;
+
+    // start two minerd processes
     let (_minerd_process, _minerd_addr) = start_minerd(tproxy_addr, None, None, false).await;
+    let (_minerd_process, _minerd_addr) = start_minerd(tproxy_addr, None, None, false).await;
+
+    // assert that two OpenExtendedMiningChannel messages are present in the queue
+    // because two minerd processes are started
+    tproxy_jdc_sniffer
+        .wait_for_message_type_and_clean_queue(
+            MessageDirection::ToUpstream,
+            MESSAGE_TYPE_OPEN_EXTENDED_MINING_CHANNEL,
+        )
+        .await;
+    tproxy_jdc_sniffer
+        .wait_for_message_type_and_clean_queue(
+            MessageDirection::ToUpstream,
+            MESSAGE_TYPE_OPEN_EXTENDED_MINING_CHANNEL,
+        )
+        .await;
+
+    jdc_pool_sniffer
+        .wait_for_message_type(MessageDirection::ToUpstream, MESSAGE_TYPE_SETUP_CONNECTION)
+        .await;
+    jdc_pool_sniffer
+        .wait_for_message_type(
+            MessageDirection::ToDownstream,
+            MESSAGE_TYPE_SETUP_CONNECTION_SUCCESS,
+        )
+        .await;
+    jdc_pool_sniffer
+        .wait_for_message_type(
+            MessageDirection::ToUpstream,
+            MESSAGE_TYPE_OPEN_EXTENDED_MINING_CHANNEL,
+        )
+        .await;
+    jdc_pool_sniffer
+        .wait_for_message_type(
+            MessageDirection::ToDownstream,
+            MESSAGE_TYPE_OPEN_EXTENDED_MINING_CHANNEL_SUCCESS,
+        )
+        .await;
+    jdc_pool_sniffer
+        .wait_for_message_type(
+            MessageDirection::ToUpstream,
+            MESSAGE_TYPE_SUBMIT_SHARES_EXTENDED,
+        )
+        .await;
+    jdc_pool_sniffer
+        .wait_for_message_type(
+            MessageDirection::ToDownstream,
+            MESSAGE_TYPE_SUBMIT_SHARES_SUCCESS,
+        )
+        .await;
+}
+
+#[tokio::test]
+async fn jd_aggregated_tproxy_integration() {
+    start_tracing();
+    let (tp, tp_addr) = start_template_provider(None, DifficultyLevel::Low);
+    let (_pool, pool_addr) = start_pool(Some(tp_addr)).await;
+    let (jdc_pool_sniffer, jdc_pool_sniffer_addr) =
+        start_sniffer("0", pool_addr, false, vec![], None);
+    let (_jds, jds_addr) = start_jds(tp.rpc_info());
+    let (_jdc, jdc_addr) = start_jdc(&[(jdc_pool_sniffer_addr, jds_addr)], tp_addr);
+    let (tproxy_jdc_sniffer, tproxy_jdc_sniffer_addr) =
+        start_sniffer("1", jdc_addr, false, vec![], None);
+    let (_translator, tproxy_addr) = start_sv2_translator(tproxy_jdc_sniffer_addr, true).await;
+
+    // start two minerd processes
+    let (_minerd_process, _minerd_addr) = start_minerd(tproxy_addr, None, None, false).await;
+    let (_minerd_process, _minerd_addr) = start_minerd(tproxy_addr, None, None, false).await;
+
+    // assert that only one OpenExtendedMiningChannel message is present in the queue
+    tproxy_jdc_sniffer
+        .wait_for_message_type_and_clean_queue(
+            MessageDirection::ToUpstream,
+            MESSAGE_TYPE_OPEN_EXTENDED_MINING_CHANNEL,
+        )
+        .await;
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    tproxy_jdc_sniffer
+        .assert_message_not_present(
+            MessageDirection::ToUpstream,
+            MESSAGE_TYPE_OPEN_EXTENDED_MINING_CHANNEL,
+        )
+        .await;
+
     jdc_pool_sniffer
         .wait_for_message_type(MessageDirection::ToUpstream, MESSAGE_TYPE_SETUP_CONNECTION)
         .await;
