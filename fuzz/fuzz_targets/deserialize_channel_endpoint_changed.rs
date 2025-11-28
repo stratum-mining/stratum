9fc46763d48f5151eebec18afced975b426db26a
Lucas Balieiro <37416577+lucasbalieiro@users.noreply.github.com>
2025-11-28 14:26:17 -0300

    introduce fuzz coverage for all `subprotocol::common_messages_sv2` messages, validating serialization <->  deserialization round-trips

---
+++ fuzz/fuzz_targets/deserialize_channel_endpoint_changed.rs
@@ -0,0 +1,23 @@
+#![no_main]
+use binary_sv2::{Deserialize, Encodable, GetSize};
+use common_messages_sv2::ChannelEndpointChanged;
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: Vec<u8>| {
+    let mut data1 = data.clone();
+    if let Ok(msg) = ChannelEndpointChanged::from_bytes(&mut data1) {
+        let mut dst = vec![0u8; msg.get_size()];
+        msg.clone().to_bytes(&mut dst).unwrap();
+
+        let mut data2 = dst.clone();
+
+        let msg2 = ChannelEndpointChanged::from_bytes(&mut data2).unwrap();
+        let mut dst2 = vec![0u8; msg2.get_size()];
+        msg2.clone().to_bytes(&mut dst2).unwrap();
+
+        // just a sanity check: if we can parse it, impl Display should also work.
+        let _ = format!("{}", msg2);
+
+        assert_eq!(msg2.eq(&msg), true);
+    }
+});
--- fuzz/fuzz_targets/deserialize_setup_connection_error.rs
+++ fuzz/fuzz_targets/deserialize_setup_connection_error.rs
@@ -1,23 +1,23 @@
 #![no_main]
 use binary_sv2::{Deserialize, Encodable, GetSize};
-use common_messages_sv2::SetupConnection;
+use common_messages_sv2::SetupConnectionError;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: Vec<u8>| {
     let mut data1 = data.clone();
-    if let Ok(setup_msg) = SetupConnection::from_bytes(&mut data1) {
+    if let Ok(setup_msg) = SetupConnectionError::from_bytes(&mut data1) {
         let mut dst = vec![0u8; setup_msg.get_size()];
         setup_msg.clone().to_bytes(&mut dst).unwrap();
 
         let mut data2 = dst.clone();
 
-        let setup_msg2 = SetupConnection::from_bytes(&mut data2).unwrap();
+        let setup_msg2 = SetupConnectionError::from_bytes(&mut data2).unwrap();
         let mut dst2 = vec![0u8; setup_msg2.get_size()];
-        setup_msg2.clone().to_bytes(&mut dst2).unwrap();
+        setup_msg2.to_bytes(&mut dst2).unwrap();
 
         // just a sanity check: if we can parse it, impl Display should also work.
         let _ = format!("{setup_msg}");
 
-        assert_eq!(setup_msg2.eq(&setup_msg), true);
+        assert_eq!(dst, dst2);
     }
 });
