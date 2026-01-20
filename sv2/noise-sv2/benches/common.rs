fda904c41fe0a8b55f4e5a5f092094ab9a787a67
bit-aloo <sshourya17@gmail.com>
2026-01-20 13:36:06 +0530

    add comments on common benchmark methods in noise

--- sv2/noise-sv2/benches/common.rs
+++ sv2/noise-sv2/benches/common.rs
@@ -2,9 +2,11 @@ use rand::{rngs::StdRng, SeedableRng};
 use secp256k1::{Keypair, Secp256k1};
 
 pub fn rng() -> StdRng {
+    // Fixed seed for deterministic benchmark runs.
     StdRng::seed_from_u64(0xdead_beef)
 }
 
+// Generates BIP340 compliant keypair
 pub fn generate_key_with_rng<R: rand::Rng + ?Sized>(rng: &mut R) -> Keypair {
     let secp = Secp256k1::new();
     let (mut secret_key, public_key) = secp.generate_keypair(rng);
