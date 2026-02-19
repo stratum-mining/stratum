bc2bca0f0ad3a270d2fdd05a759659e1cbe02295
Lucas Balieiro <37416577+lucasbalieiro@users.noreply.github.com>
2026-02-19 13:21:34 -0300

    make an enum based selector in the fuzz targets

--- fuzz/fuzz_targets/end_to_end_serialization_for_common_messages.rs
+++ fuzz/fuzz_targets/end_to_end_serialization_for_common_messages.rs
@@ -1,15 +1,36 @@
 #![no_main]
 
 mod common;
+use arbitrary::Arbitrary;
 use binary_sv2::{Deserialize, GetSize, Serialize};
 use common_messages_sv2::*;
-
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|data: Vec<u8>| {
-    test_roundtrip!(SetupConnection, data);
-    test_roundtrip!(SetupConnectionError, data);
-    test_roundtrip!(SetupConnectionSuccess, data);
-    test_roundtrip!(Reconnect, data);
-    test_roundtrip!(ChannelEndpointChanged, data);
+#[derive(Arbitrary, Debug)]
+enum FuzzInput {
+    SetupConnection(Vec<u8>),
+    SetupConnectionError(Vec<u8>),
+    SetupConnectionSuccess(Vec<u8>),
+    Reconnect(Vec<u8>),
+    ChannelEndpointChanged(Vec<u8>),
+}
+
+fuzz_target!(|input: FuzzInput| {
+    match input {
+        FuzzInput::SetupConnection(data) => {
+            test_roundtrip!(SetupConnection, data)
+        }
+        FuzzInput::SetupConnectionError(data) => {
+            test_roundtrip!(SetupConnectionError, data)
+        }
+        FuzzInput::SetupConnectionSuccess(data) => {
+            test_roundtrip!(SetupConnectionSuccess, data)
+        }
+        FuzzInput::Reconnect(data) => {
+            test_roundtrip!(Reconnect, data)
+        }
+        FuzzInput::ChannelEndpointChanged(data) => {
+            test_roundtrip!(ChannelEndpointChanged, data)
+        }
+    }
 });
